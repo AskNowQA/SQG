2f51a20a75c2305a9a1c904857131c3577e70f14
hamidzafar <hamid.zafar.tud@gmail.com>
2017-08-25 15:58:15 +0200

    parser for lc_quad_linked

---
+++ parser/lc_quad_linked.py
@@ -0,0 +1,50 @@
+import json, re
+from qapair import QApair
+from answer import Answer
+from uri import Uri
+
+class LC_Qaud_Linked:	
+	def __init__(self, path = "./data/LC-QUAD/linked.json"):
+		self.raw_data = []
+		self.qapairs = []
+		self.path = path
+
+	def load(self):
+		with open(self.path) as data_file:	
+			self.raw_data = json.load(data_file)
+
+	def parse(self):
+		parser = LC_QaudParser()
+		for raw_row in self.raw_data:
+			self.qapairs.append(QApair(raw_row["question"], "", raw_row["sparql_query"], raw_row, parser))
+
+	def print_pairs(self, n = -1):
+		for item in self.qapairs[0:n]:
+			print item
+			print ""
+
+
+class LC_QaudParser:
+	def parse_question(self, raw_question):
+		return raw_question
+
+	def parse_sparql(self, raw_query):
+		uris = [Uri(raw_uri, self.parse_uri) for raw_uri in re.findall('<[^>]*>',raw_query)]
+
+		return raw_query, True, uris
+
+	def parse_answers(self, raw_answers):		
+			return []
+
+	def parse_answer(self, answer_type, raw_answer):
+		return "",""
+
+	def parse_uri(self, raw_uri):
+		if raw_uri.find("/resource/") >= 0:
+			return "?s", raw_uri
+		elif raw_uri.find("/ontology/") >= 0 or raw_uri.find("/property/") >= 0:
+			return "?p", raw_uri
+		elif raw_uri.find("rdf-syntax-ns#type") >= 0:
+			return "?t", raw_uri
+		else:
+			return "?u", raw_uri
