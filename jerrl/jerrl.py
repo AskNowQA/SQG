b6d4edcc9495bbc20e8b39114d5e46f1b12043d1
hamidzafar <hamid.zafar.tud@gmail.com>
2017-08-25 16:51:29 +0200

    add linkeditem and uri parsers

--- jerrl/jerrl.py
+++ jerrl/jerrl.py
@@ -1,3 +1,6 @@
+from common.linkeditem import LinkedItem
+from common.uri import Uri
+
 class Jerrl:
 	def __init__(self):
 		pass
@@ -5,5 +8,18 @@ class Jerrl:
 	def do(self, qapair):
 		items = []
 		for raw_item in qapair.raw_row["predicate mapping"]:
-			items.append()
+			items.append(LinkedItem(raw_item, self.parse_linkeditem))
 		return items
+
+	def parse_linkeditem(self, raw_item):
+		return Uri(raw_item["uri"], self.parse_uri), raw_item["seq"], raw_item["label"]
+	
+	def parse_uri(self, raw_uri):
+		if raw_uri.find("/resource/") >= 0:
+			return "?s", raw_uri
+		elif raw_uri.find("/ontology/") >= 0 or raw_uri.find("/property/") >= 0:
+			return "?p", raw_uri
+		elif raw_uri.find("rdf-syntax-ns#type") >= 0:
+			return "?t", raw_uri
+		else:
+			return "?u", raw_uri
