c7f96be31627ab8cec7a37b5ba94bc3f7716dffb
hamidzafar <hamid.zafar.tud@gmail.com>
2017-08-25 16:51:05 +0200

    call parser and save the results

--- common/linkeditem.py
+++ common/linkeditem.py
@@ -2,5 +2,8 @@ class LinkedItem:
 	def __init__(self, raw_item, parser):
 		self.raw_item = raw_item
 		self.uri, self.positions, self.label = parser(raw_item)
+
+	def __str__(self):
+		return self.uri.__str__().encode("ascii","ignore")
 		
 
