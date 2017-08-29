6379204ceb71cc00519e7df2959c1c4648e1cfde
hamidzafar <hamid.zafar.tud@gmail.com>
2017-08-29 17:25:37 +0200

    bug fix: __str__

--- common/answer.py
+++ common/answer.py
@@ -1,9 +1,11 @@
 class Answer:
 	def __init__(self, answer_type, raw_answer, parser):
 		self.raw_answer = raw_answer
-		self.type, self.text = parser(answer_type, raw_answer)
+		self.type, self.answer = parser(answer_type, raw_answer)
 
 	def __str__(self):
-		if type(self.text) is bool:
-			return str(self.text)
-		return self.text.encode("ascii","ignore")
+		if self.type == "bool":
+			return str(self.answer)
+		elif self.type == "uri":
+			return self.answer.__str__().encode("ascii", "ignore")
+		return self.answer.encode("ascii", "ignore")
