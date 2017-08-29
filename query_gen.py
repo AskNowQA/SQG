832a4866ee659bcb895773bb5274d6ade9241c5b
hamidzafar <hamid.zafar.tud@gmail.com>
2017-08-29 10:23:04 +0200

    refactor

--- query_gen.py
+++ query_gen.py
@@ -10,7 +10,24 @@ def prepare_dataset(ds):
 
 if __name__ == "__main__":
 	jerrl = Jerrl()
-	for qapair in prepare_dataset(LC_Qaud_Linked()).qapairs:
-		print jerrl.do(qapair)
-		break
-	
+	i=0
+	no_answer = 0
+	ds = LC_Qaud_Linked(path = "./data/LC-QUAD/linked_answer3.json")
+	tmp = []
+	for qapair in prepare_dataset(ds).qapairs:
+		results = jerrl.do(qapair)
+		# if len(results) != 2:
+		# 	continue
+		# sparql = qapair.sparql
+		# print qapair.question.text
+		# print qapair.question_template(results)
+		# print sparql.where_clause
+		if qapair.answers is None or len(qapair.answers) == 0:
+			no_answer += 1
+		# print qapair.answers
+		# print "--"
+		
+		i+=1
+		# if i > 10:
+		# 	break
+	print no_answer, i
