acd815167e24f41ddf44ddd8c023ec04a5639792
hamidzafar <hamid.zafar.tud@gmail.com>
2017-08-31 16:19:23 +0200

    override equality operator

--- common/uri.py
+++ common/uri.py
@@ -1,16 +1,27 @@
 class Uri:
 	def __init__(self, raw_uri, parser):
 		self.raw_uri = raw_uri
-		self.type, self.uri = parser(raw_uri) 
+		self.uri_type, self.uri = parser(raw_uri)
+
+	def __eq__(self, other):
+		if isinstance(other, Uri):
+			return self.uri_type == other.uri_type and self.uri == other.uri
+		return NotImplemented
 
 	def is_entity(self):
-		return self.type == "?s"
+		return self.uri_type == "?s"
 
 	def is_ontology(self):
-		return self.type == "?p"
+		return self.uri_type == "?p"
 
 	def sparql_format(self):
+		if self.uri_type == "g":
+			return self.uri
 		return u"<{}>".format(self.uri)
 
 	def __str__(self):
-		return self.uri.encode("ascii", "ignore")
+		return "{}:{}".format(self.uri_type, self.uri[self.uri.rfind("/") + 1:].encode("ascii", "ignore"))
+
+	@staticmethod
+	def generic_uri(var_num):
+		return Uri("g", lambda r: ("g", "?u_{}".format(var_num)))
