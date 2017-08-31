fa83f2eedb7b858b793a370ed8f62bc4d6325897
hamidzafar <hamid.zafar.tud@gmail.com>
2017-08-31 16:15:58 +0200

    rename property type to answer_type

--- common/answer.py
+++ common/answer.py
@@ -1,11 +1,12 @@
 class Answer:
 	def __init__(self, answer_type, raw_answer, parser):
 		self.raw_answer = raw_answer
-		self.type, self.answer = parser(answer_type, raw_answer)
+		self.answer_type, self.answer = parser(answer_type, raw_answer)
 
 	def __str__(self):
-		if self.type == "bool":
+		if self.answer_type == "bool":
 			return str(self.answer)
-		elif self.type == "uri":
+		elif self.answer_type == "uri":
 			return self.answer.__str__().encode("ascii", "ignore")
+
 		return self.answer.encode("ascii", "ignore")
