dc141a8cdbdd0d743e9ba7eac769d1acf6b9f7cf
hamidzafar <hamid.zafar.tud@gmail.com>
2017-08-31 16:16:45 +0200

    override equality operator

--- common/graph/node.py
+++ common/graph/node.py
@@ -1,8 +1,14 @@
+
 class Node:
     def __init__(self, uri):
         self.uri = uri
         self.inbound = []
         self.outbound = []
 
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            return self.uri == other.uri
+        return NotImplemented
+
     def __str__(self):
-        return self.uri.sparql_format().encode("ascii", "ignore")
+        return self.uri.__str__()
