15d81d7e737eb9be818e344d425c1f5ca98339ad
hamidzafar <hamid.zafar.tud@gmail.com>
2017-08-31 16:15:14 +0200

    rename answers to answerser

---
+++ common/answerset.py
@@ -0,0 +1,13 @@
+from answer import Answer
+
+class Answers:
+	def __init__(self, raw_answers, parser):
+		self.raw_answers = raw_answers
+		self.answers = []		
+		self.answers = parser(raw_answers)
+
+	def __len__(self):
+		return len(self.answers)
+
+	def __str__(self):
+		return "\n".join(str(a) for a in self.answers)
