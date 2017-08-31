2690ba7ddfe5b6bbaaa121e8a8116a917d6e0999
hamidzafar <hamid.zafar.tud@gmail.com>
2017-08-31 16:20:44 +0200

    flow to generate single relation queries

--- query_gen.py
+++ query_gen.py
@@ -1,6 +1,7 @@
 from parser.lc_quad_linked import LC_Qaud_Linked
 from jerrl.jerrl import Jerrl
 from dbpedia.dbpedia import DBpedia
+from common.graph.paths import Paths
 
 
 def prepare_dataset(ds):
@@ -15,14 +16,18 @@ def qg(qapair):
 	ontologies = [u for u in qapair.sparql.uris if u.is_ontology()]
 
 	answer_uri = []
-	for answer in qapair.answers.answers:
-		if answer.type == "uri":
-			answer_uri.append(answer.answer)
-			entities.append(answer.answer)
+	for answer_set in qapair.answers.answers:
+		for answer in answer_set:
+			if answer.answer_type == "uri":
+				answer_uri.append(answer.answer)
+				entities.append(answer.answer)
 
-	results = []
 	dbp = DBpedia()
 
+	path_set = Paths()
+	if len(entities) > 100:
+		# print "len(entities)", len(entities)
+		return False
 	for ent1 in entities:
 		for ent2 in entities:
 			if ent1 == ent2 or (ent1 in answer_uri and ent2 in answer_uri):
@@ -30,11 +35,25 @@ def qg(qapair):
 			paths = dbp.find_minimal_subgraph(ent1, ent2, max_hop=2)
 			if paths is not None and len(paths) > 0:
 				for path in paths:
-					print path
+					path_set.add_path(path)
 
-	for r in results:
-		print r
-	print len(entities), len(ontologies)
+	# for path in path_set.paths:
+	# 	print path
+	# print "-----"
+
+	path_set.prune_paths(qapair.answers)
+	path_set.replace_answers(qapair.answers)
+	path_set.merge_paths()
+
+	if len(path_set.paths) == 1:
+		q = path_set.paths[0].generate_sparql()
+		print q
+		print dbp.query(q)
+	# for path in path_set.paths:
+	# 	print path
+	# 	print path.generate_sparql()
+
+	# print len(entities), len(ontologies)
 
 if __name__ == "__main__":
 	jerrl = Jerrl()
@@ -44,8 +63,9 @@ if __name__ == "__main__":
 	tmp = []
 	for qapair in prepare_dataset(ds).qapairs:
 		i += 1
-		if i == 1:
-			continue
+		print i
+		# if i <= 40:
+		# 	continue
 		results = jerrl.do(qapair)
 		# if len(results) != 2:
 		# 	continue
@@ -56,9 +76,6 @@ if __name__ == "__main__":
 			# print qapair.answers
 			print "--"
 
-		# print "--"
-		
-
-		if i > 10:
+		if i > 15:
 			break
 	print no_answer, i
