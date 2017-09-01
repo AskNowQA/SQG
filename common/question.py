365cc55c9d95bd4c5de2112b1b0efbf712769266
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-01 12:13:05 +0200

    remove unused imports

--- common/question.py
+++ common/question.py
@@ -1,11 +1,8 @@
-from answers import Answers
 class Question:
-
 	def __init__(self, raw_question, parser):
 		self.text = ""
 		self.raw_question = raw_question
 		self.text = parser(raw_question)
-
 
 	def __str__(self):
 		return self.text.encode("ascii","ignore")
