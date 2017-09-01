f3bc177896c9bc258854d0daaa411ddf95cbbebb
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-01 12:10:24 +0200

    refactor

--- common/qapair.py
+++ common/qapair.py
@@ -3,23 +3,20 @@ from answerset import AnswerSet
 from sparql import SPARQL
 
 class QApair:
-	def __init__(self, raw_question, raw_answers, raw_query, raw_row, parser):
+	def __init__(self, raw_question, raw_answerset, raw_query, raw_row, parser):
 		self.raw_row = raw_row
 		self.question = []
-		self.answers = []
 		self.sparql = []
 		
 		self.question = Question(raw_question, parser.parse_question)
-		self.answers = AnswerSet(raw_answers, parser.parse_answers)
+		self.answerset = AnswerSet(raw_answerset, parser.parse_answerset)
 		self.sparql = SPARQL(raw_query, parser.parse_sparql)
 
 	def question_template(self, entity_relation_list):
 		question = self.question.text.lower()
 		for item in entity_relation_list:
 			question = question.replace(item.label.lower(), item.uri.type)
-
 		return question
 
-
-	def __str__(self):		
-		return "{}\n{}\n{}".format(self.question, self.answers, self.sparql)
+	def __str__(self):
+		return "{}\n{}\n{}".format(self.question, self.answerset, self.sparql)
