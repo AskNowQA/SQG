7b8cdc81ec005e6771c24b3f6eba6ed8a304d566
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-01 12:08:48 +0200

    refactor+override equality operator

--- common/answerset.py
+++ common/answerset.py
@@ -1,13 +1,25 @@
-from answer import Answer
+class AnswerSet:
+	def __init__(self, raw_answerset, parser):
+		self.raw_answerset = raw_answerset
+		self.answer_rows = []
+		self.answer_rows = parser(raw_answerset)
 
-class Answers:
-	def __init__(self, raw_answers, parser):
-		self.raw_answers = raw_answers
-		self.answers = []		
-		self.answers = parser(raw_answers)
+	def __eq__(self, other):
+		if isinstance(other, AnswerSet):
+			if len(self.answer_rows) != len(other.answer_rows):
+				return False
+			for answers in self.answer_rows:
+				found = False
+				for other_answers in other.answer_rows:
+					if answers == other_answers:
+						found = True
+				if not found:
+					return False
+			return True
+		return NotImplemented
 
 	def __len__(self):
-		return len(self.answers)
+		return len(self.answer_rows)
 
 	def __str__(self):
 		return "\n".join(str(a) for a in self.answers)
