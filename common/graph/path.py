57deb4ac6f2dd5c72c05bf64cf39cbebe4ee9d62
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-04 14:35:27 +0200

    two way edge, expand method

--- common/graph/path.py
+++ common/graph/path.py
@@ -4,10 +4,22 @@ from common.uri import Uri
 
 
 class Path:
-    def __init__(self, source_node, edge, dest_node):
+    def __init__(self, source_node, source_to_dest_edge, dest_to_source_edge, dest_node):
         self.source_node = source_node
-        self.edge = edge
+        self.source_to_dest_edge = source_to_dest_edge
+        self.dest_to_source_edge = dest_to_source_edge
         self.dest_node = dest_node
+
+    def copy(self):
+        return Path(self.source_node, self.source_to_dest_edge, self.dest_to_source_edge, self.dest_node)
+
+    def expand(self, other_path):
+        if isinstance(other_path.dest_node, Node) and isinstance(self.dest_node, Node) \
+             and self.dest_node == other_path.dest_node:
+            new_path = self.copy()
+            new_path.dest_node = Path(self.dest_node, None, other_path.source_to_dest_edge, other_path.source_node)
+            return new_path
+        return None
 
     def merge(self, other_path):
         """
@@ -15,20 +27,31 @@ class Path:
         :param other_path: should be of type common.graph.Path
         :return: True if it succeed
         """
-        if self.source_node == other_path.source_node and self.edge == other_path.edge:
-            if self.dest_node == other_path.dest_node:
+        if self.source_to_dest_edge == other_path.source_to_dest_edge:
+            if self.source_node == other_path.source_node:
+                if self.dest_node == other_path.dest_node:
+                    return True
+                elif isinstance(other_path.dest_node, Path) and isinstance(self.dest_node, Node) \
+                        and self.dest_node.mergable and self.dest_node == other_path.dest_node.source_node:
+                    self.dest_node = other_path.dest_node
+                    return True
+                elif isinstance(other_path.dest_node, Node) and isinstance(self.dest_node, Path) \
+                        and other_path.dest_node.mergable and other_path.dest_node == self.dest_node.source_node:
+                    return True
+                elif isinstance(other_path.dest_node, Path) and isinstance(self.dest_node, Path):
+                    return self.dest_node.merge(other_path.dest_node)
+                elif isinstance(other_path.dest_node, Node) and isinstance(self.dest_node, Node) \
+                        and self.dest_node.mergable and other_path.dest_node.mergable:
+                    self.dest_node = Node([self.dest_node.uris, other_path.dest_node.uris])
+                    return True
+            else:
+                if isinstance(other_path.source_node, Node) and isinstance(self.source_node, Node) \
+                        and self.source_node.mergable and other_path.source_node.mergable \
+                        and isinstance(other_path.dest_node, Node) and isinstance(self.dest_node, Node) \
+                        and self.dest_node.mergable and other_path.dest_node.mergable:
+                    self.source_node = Node([self.source_node.uris, other_path.source_node.uris], True)
+                    self.dest_node = Node([self.dest_node.uris, other_path.dest_node.uris], True)
                 return True
-            elif isinstance(other_path.dest_node, Path) and isinstance(self.dest_node, Node) \
-                    and self.dest_node == other_path.dest_node.source_node:
-                self.dest_node = other_path.dest_node
-                return True
-            elif isinstance(other_path.dest_node, Node) and isinstance(self.dest_node, Path) \
-                    and other_path.dest_node == self.dest_node.source_node:
-                return True
-
-            elif isinstance(other_path.dest_node, Path) and isinstance(self.dest_node, Path):
-                return self.dest_node.merge(other_path.dest_node)
-
         return False
 
     def contains_answers(self, answers):
@@ -39,7 +62,7 @@ class Path:
         """
         to_be_removed = []
         for answer in answers:
-            if (self.source_node.uri == answer.answer) or (isinstance(self.dest_node, Node) and self.dest_node.uri == answer.answer):
+            if (self.source_node.has_uri(answer.answer)) or (isinstance(self.dest_node, Node) and self.dest_node.has_uri(answer.answer)):
                 to_be_removed.append(answer)
 
         answers = [answer for answer in answers if answer not in to_be_removed]
@@ -59,14 +82,12 @@ class Path:
         """
         for answer_row in answers_set.answer_rows:
             for answer in answer_row.answers:
-                if self.source_node.uri == answer.answer:
-                    self.source_node.uri = Uri.generic_uri(var_num)
+                if self.source_node.replace_uri(answer.answer, Uri.generic_uri(var_num)):
                     var_num += 1
         if isinstance(self.dest_node, Node):
             for answer_row in answers_set.answer_rows:
                 for answer in answer_row.answers:
-                    if self.dest_node.uri == answer.answer:
-                        self.dest_node.uri = Uri.generic_uri(var_num)
+                    if self.dest_node.replace_uri(answer.answer, Uri.generic_uri(var_num)):
                         var_num += 1
         else:
             self.dest_node.replace_answers(answers_set, var_num)
@@ -75,32 +96,45 @@ class Path:
         dest_node = self.dest_node
         if isinstance(self.dest_node, Path):
             dest_node = self.dest_node.source_node
-            return u" {} {} {} . {}".format(self.source_node.uri.sparql_format(), self.edge.uri.sparql_format(),
-                                       dest_node.uri.sparql_format(), self.dest_node.__generate_where())
+            return u" {} {} {} . {}".format(self.source_node.sparql_format(), self.source_to_dest_edge.sparql_format(),
+                                            dest_node.sparql_format(), self.dest_node.__generate_where())
         else:
-            return u" {} {} {} ".format(self.source_node.uri.sparql_format(), self.edge.uri.sparql_format(), dest_node.uri.sparql_format())
+            if self.dest_to_source_edge is None:
+                return u" {} {} {} ".format(self.source_node.sparql_format(), self.source_to_dest_edge.sparql_format(), dest_node.sparql_format())
+            else:
+                return u" {} {} {} ".format(self.dest_node.uri.sparql_format(),
+                                            self.dest_to_source_edge.sparql_format(), self.source_node.sparql_format())
 
     def generate_sparql(self):
-        return u"SELECT DISTINCT * WHERE {{ {} }}".format(self.__generate_where())
+        try:
+            return u"SELECT DISTINCT * WHERE {{ {} }}".format(self.__generate_where())
+        except:
+            return None
 
     def __str__(self):
-        return u"{} --> {} --> {}".format(self.source_node.__str__(), self.edge.__str__(),
+        if self.dest_to_source_edge is None:
+            return u"{} --> {} --> {}".format(self.source_node.__str__(), self.source_to_dest_edge.__str__(),
+                                          self.dest_node.__str__()).encode("ascii", "ignore")
+        else:
+            return u"{} <-- {} <-- {}".format(self.source_node.__str__(), self.dest_to_source_edge.__str__(),
                                           self.dest_node.__str__()).encode("ascii", "ignore")
 
     @staticmethod
-    def create_path(uris):
+    def create_path(uris, is_answer):
         """
         Create a path from the input URIs
         :param uris: should type of [common.URI]
         :return: common.graph.Path
         """
+        mergable = lambda uri: True if is_answer(uri) else False
+
         if len(uris) == 3:
-            source_node = Node(uris[0])
-            dest_node = Node(uris[2])
+            source_node = Node(uris[0], mergable(uris[0]))
+            dest_node = Node(uris[2], mergable(uris[2]))
             edge = Edge(source_node, uris[1], dest_node)
-            return Path(source_node, edge, dest_node)
+            return Path(source_node, edge, None, dest_node)
         else:
-            source_node = Node(uris[0])
-            dest_node = Path.create_path(uris[2:])
+            source_node = Node(uris[0], mergable(uris[0]))
+            dest_node = Path.create_path(uris[2:], is_answer)
             edge = Edge(source_node, uris[1], dest_node)
-            return Path(source_node, edge, dest_node)
+            return Path(source_node, edge, None, dest_node)
