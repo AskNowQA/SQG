eddd840d009a44c247b52b2223ab611ed70f1723
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-04 14:33:48 +0200

    can have multiple uris

--- common/graph/node.py
+++ common/graph/node.py
@@ -1,14 +1,57 @@
+from common.uri import Uri
+
 
 class Node:
-    def __init__(self, uri):
-        self.uri = uri
+    def __init__(self, uris, mergable=False):
+        if isinstance(uris, Uri):
+            self.uris = [uris]
+        elif isinstance(uris, list):
+            self.uris = []
+            for uri in uris:
+                if isinstance(uri, Uri):
+                    self.uris.append(uri)
+                elif isinstance(uris, list):
+                    self.uris.extend(uri)
+        self.uris = list(set(self.uris))
+        self.mergable = mergable
         self.inbound = []
         self.outbound = []
 
+    def __all_generic_uris(self):
+        uris_type = set([u.uri_type for u in self.uris])
+        return len(uris_type) == 1 and uris_type.pop() == "g"
+
+    def replace_uri(self, uri, new_uri):
+        for i in range(len(self.uris)):
+            if self.uris[i] == uri:
+                self.uris[i] = new_uri
+                if self.__all_generic_uris():
+                    self.uris = [new_uri]
+                return True
+        return False
+
+    def has_uri(self, uri):
+        for u in self.uris:
+            if u == uri:
+                return True
+        return False
+
+    def sparql_format(self):
+        if len(self.uris) == 1:
+            return self.uris[0].sparql_format()
+        raise Exception("...")
+
     def __eq__(self, other):
         if isinstance(other, Node):
-            return self.uri == other.uri
+            for uri in self.uris:
+                if not other.has_uri(uri):
+                    return False
+
+            return True
         return NotImplemented
 
     def __str__(self):
-        return self.uri.__str__()
+        output = []
+        for uri in self.uris:
+            output.append(uri.__str__())
+        return output
