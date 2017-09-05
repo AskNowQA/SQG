4f45e57b52b1e6bef4f22855058ebebc86b96d15
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-05 13:54:55 +0200

    add contains_node method

--- common/graph/path.py
+++ common/graph/path.py
@@ -1,6 +1,7 @@
 from node import Node
 from edge import Edge
 from common.uri import Uri
+import sys
 
 
 class Path:
@@ -15,7 +16,8 @@ class Path:
 
     def expand(self, other_path):
         if isinstance(other_path.dest_node, Node) and isinstance(self.dest_node, Node) \
-             and self.dest_node == other_path.dest_node:
+             and self.dest_node == other_path.dest_node\
+             and not self.contains_node(other_path.source_node):
             new_path = self.copy()
             new_path.dest_node = Path(self.dest_node, None, other_path.source_to_dest_edge, other_path.source_node)
             return new_path
@@ -27,31 +29,49 @@ class Path:
         :param other_path: should be of type common.graph.Path
         :return: True if it succeed
         """
-        if self.source_to_dest_edge == other_path.source_to_dest_edge:
+        if (self.source_to_dest_edge == other_path.source_to_dest_edge and self.source_to_dest_edge is not None) or \
+                (self.dest_to_source_edge == other_path.dest_to_source_edge and self.dest_to_source_edge is not None):
             if self.source_node == other_path.source_node:
                 if self.dest_node == other_path.dest_node:
                     return True
                 elif isinstance(other_path.dest_node, Path) and isinstance(self.dest_node, Node) \
-                        and self.dest_node.mergable and self.dest_node == other_path.dest_node.source_node:
+                        and self.dest_node.mergable and self.dest_node <= other_path.dest_node.source_node:
                     self.dest_node = other_path.dest_node
                     return True
+
                 elif isinstance(other_path.dest_node, Node) and isinstance(self.dest_node, Path) \
-                        and other_path.dest_node.mergable and other_path.dest_node == self.dest_node.source_node:
+                        and other_path.dest_node.mergable and other_path.dest_node <= self.dest_node.source_node:
                     return True
+
                 elif isinstance(other_path.dest_node, Path) and isinstance(self.dest_node, Path):
                     return self.dest_node.merge(other_path.dest_node)
+
                 elif isinstance(other_path.dest_node, Node) and isinstance(self.dest_node, Node) \
                         and self.dest_node.mergable and other_path.dest_node.mergable:
-                    self.dest_node = Node([self.dest_node.uris, other_path.dest_node.uris])
+                    self.dest_node = Node([self.dest_node.uris, other_path.dest_node.uris], True)
                     return True
             else:
                 if isinstance(other_path.source_node, Node) and isinstance(self.source_node, Node) \
                         and self.source_node.mergable and other_path.source_node.mergable \
-                        and isinstance(other_path.dest_node, Node) and isinstance(self.dest_node, Node) \
-                        and self.dest_node.mergable and other_path.dest_node.mergable:
-                    self.source_node = Node([self.source_node.uris, other_path.source_node.uris], True)
-                    self.dest_node = Node([self.dest_node.uris, other_path.dest_node.uris], True)
+                        and isinstance(other_path.dest_node, Node) and isinstance(self.dest_node, Node):
+                    if self.dest_node.mergable and other_path.dest_node.mergable:
+                        self.source_node = Node([self.source_node.uris, other_path.source_node.uris], True)
+                        self.dest_node = Node([self.dest_node.uris, other_path.dest_node.uris], True)
+                        return True
+                    elif self.dest_node == other_path.dest_node:
+                        self.source_node = Node([self.source_node.uris, other_path.source_node.uris], True)
+                        return True
+
+        return False
+
+    def contains_node(self, node):
+        if self.source_node == node:
+            return True
+        if isinstance(self.dest_node, Node):
+            if self.dest_node == node:
                 return True
+        elif isinstance(self.dest_node, Path):
+            return self.dest_node.contains_node(node)
         return False
 
     def contains_answers(self, answers):
@@ -102,13 +122,14 @@ class Path:
             if self.dest_to_source_edge is None:
                 return u" {} {} {} ".format(self.source_node.sparql_format(), self.source_to_dest_edge.sparql_format(), dest_node.sparql_format())
             else:
-                return u" {} {} {} ".format(self.dest_node.uri.sparql_format(),
+                return u" {} {} {} ".format(self.dest_node.sparql_format(),
                                             self.dest_to_source_edge.sparql_format(), self.source_node.sparql_format())
 
     def generate_sparql(self):
         try:
             return u"SELECT DISTINCT * WHERE {{ {} }}".format(self.__generate_where())
         except:
+            print("Unexpected error:", sys.exc_info()[0])
             return None
 
     def __str__(self):
