e51f1f9818ef95823c7c589a513af583d96e3864
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-07 16:48:57 +0200

    add equal and generic_equal methods

--- common/uri.py
+++ common/uri.py
@@ -3,10 +3,8 @@ class Uri:
 		self.raw_uri = raw_uri
 		self.uri_type, self.uri = parser(raw_uri)
 
-	def __eq__(self, other):
-		if isinstance(other, Uri):
-			return self.uri_type == other.uri_type and self.uri == other.uri
-		return NotImplemented
+	def is_generic(self):
+		return self.uri_type == "g"
 
 	def is_entity(self):
 		return self.uri_type == "?s"
@@ -18,6 +16,14 @@ class Uri:
 		if self.uri_type == "g":
 			return self.uri
 		return u"<{}>".format(self.uri)
+
+	def generic_equal(self, other):
+		return (self.is_generic() and other.is_generic()) or self == other
+
+	def __eq__(self, other):
+		if isinstance(other, Uri):
+			return self.uri_type == other.uri_type and self.uri == other.uri
+		return NotImplemented
 
 	def __hash__(self):
 		return hash(self.__str__())
