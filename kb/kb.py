adace1da96d43439b2fc3d6b0e2a2b2bd7f0e217
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-13 09:53:25 +0200

    instead of answer, return the number of matching answer

--- kb/kb.py
+++ kb/kb.py
@@ -5,6 +5,7 @@ class KB(object):
     def __init__(self, endpoint, default_graph_uri=""):
         self.endpoint = endpoint
         self.default_graph_uri = default_graph_uri
+        self.type_uri = "type_uri"
 
     def query(self, q):
         payload = (
@@ -15,8 +16,12 @@ class KB(object):
         r = requests.get(self.endpoint, params=payload)
         return r.status_code, r.json() if r.status_code == 200 else None
 
-    def query_where(self, clauses):
-        query = u"SELECT DISTINCT * WHERE {{ {} }}".format(" .".join(clauses))
+    def query_where(self, clauses, count=False):
+        where = u"WHERE {{ {} }}".format(" .".join(clauses))
+        if count:
+            query = u"SELECT COUNT(DISTINCT *) {}".format(where)
+        else:
+            query = u"SELECT DISTINCT * {}".format(where)
         status, response = self.query(query)
         if status == 200 and len(response["results"]["bindings"]) > 0:
             return response
@@ -27,7 +32,8 @@ class KB(object):
 UNION {{ values ?m {{ 1 }} {1} {0} ?u1 }}
 UNION {{ values ?m {{ 2 }} {1} ?u1 {0} }}
 UNION {{ values ?m {{ 3 }} {0} ?u1 {1} }}
-}}""".format(relation_uri, entity_uri)
+UNION {{ values ?m {{ 4 }} ?u1 {2} {0} }}
+}}""".format(relation_uri, entity_uri, self.type_uri)
         status, response = self.query(query)
         if status == 200 and len(response["results"]["bindings"]) > 0:
             return response["results"]["bindings"]
