ae75ae8fd83e73d8ac94f01638d020b370d1629c
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-14 11:54:24 +0200

    add prefix to query

--- kb/kb.py
+++ kb/kb.py
@@ -19,25 +19,33 @@ class KB(object):
     def query_where(self, clauses, count=False):
         where = u"WHERE {{ {} }}".format(" .".join(clauses))
         if count:
-            query = u"SELECT COUNT(DISTINCT *) {}".format(where)
+            query = u"{} SELECT COUNT(DISTINCT *) {}".format(self.prefix(), where)
         else:
-            query = u"SELECT DISTINCT * {}".format(where)
+            query = u"{} SELECT DISTINCT * {}".format(self.prefix(), where)
         status, response = self.query(query)
         if status == 200 and len(response["results"]["bindings"]) > 0:
             return response
 
     def one_hop_graph(self, entity_uri, relation_uri):
-        query = u"""SELECT DISTINCT ?m, count(?u1) WHERE {{
+        query = u"""{3} 
+SELECT DISTINCT ?m, count(?u1) WHERE {{
 {{ values ?m {{ 0 }} ?u1 {0} {1} }}
 UNION {{ values ?m {{ 1 }} {1} {0} ?u1 }}
 UNION {{ values ?m {{ 2 }} {1} ?u1 {0} }}
 UNION {{ values ?m {{ 3 }} {0} ?u1 {1} }}
 UNION {{ values ?m {{ 4 }} ?u1 {2} {0} }}
-}}""".format(relation_uri, entity_uri, self.type_uri)
+}}""".format(self.uri_to_sparql(relation_uri), self.uri_to_sparql(entity_uri), self.type_uri, self.prefix())
         status, response = self.query(query)
         if status == 200 and len(response["results"]["bindings"]) > 0:
             return response["results"]["bindings"]
 
+    def prefix(self):
+        return ""
+
     @staticmethod
     def parse_uri(input_uri):
         pass
+
+    @staticmethod
+    def uri_to_sparql(input_uri):
+        return input_uri.uri
