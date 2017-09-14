aef91bf297bd32ae0508cce7905a612ab3711d95
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-14 12:56:53 +0200

    add prefix methods

--- kb/freebase.py
+++ kb/freebase.py
@@ -7,19 +7,26 @@ class Freebase(KB):
 		super(Freebase, self).__init__(endpoint)
 		self.type_uri = "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"
 
-	def prefix(self):
-		return "PREFIX ns: <http://rdf.freebase.com/ns/>"
+	@staticmethod
+	def shorten_prefix():
+		return "ns:"
+
+	@staticmethod
+	def prefix():
+		return "http://rdf.freebase.com/ns/"
+
+	@staticmethod
+	def query_prefix():
+		return "PREFIX {} <{}>".format(Freebase.shorten_prefix(), Freebase.prefix())
 
 	@staticmethod
 	def parse_uri(input_uri):
 		if isinstance(input_uri, bool):
 			return "bool", input_uri
 		raw_uri = input_uri.strip("<>")
-		if raw_uri.find("/resource/") >= 0:
+		if raw_uri.find("ns:m.") >= 0:
 			return "?s", raw_uri
-		elif raw_uri.find("/ontology/") >= 0 or raw_uri.find("/property/") >= 0:
+		elif raw_uri.find("ns:") >= 0:
 			return "?p", raw_uri
-		elif raw_uri.find("rdf-syntax-ns#type") >= 0:
-			return "?t", raw_uri
 		else:
-			return "?u", raw_uri
+			return raw_uri, raw_uri
