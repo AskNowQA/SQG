39888d37b967779a19f8ec9507582d31b503849b
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-15 17:50:00 +0200

    refactor

--- query_gen.py
+++ query_gen.py
@@ -7,20 +7,12 @@ from common.graph.graph import Graph
 import json
 
 
-def prepare_dataset(ds):
-	ds.load()
-	ds.parse()
-	return ds
 
 def qg(kb, parser, qapair):
-	if qapair.answerset.number_of_answer() != 1:
-		return False
-
-
-	ask_query = "ASK " in qapair.sparql.query
-
 	print qapair.sparql
 	print qapair.question
+
+	ask_query = "ASK " in qapair.sparql.query
 	all_uri = []
 	entities = set([u for u in qapair.sparql.uris if u.is_entity()])
 	ontologies = set([u for u in qapair.sparql.uris if u.is_ontology()])
@@ -40,54 +32,56 @@ def qg(kb, parser, qapair):
 	print graph
 	print "-----"
 	where = graph.to_where_statement()
-	if len(where) == 0:
-		return False
-	where = where[0]
-	print graph
-	print where
-	raw_answer = kb.query_where(where, ask=ask_query)
-	answerset = AnswerSet(raw_answer, parser.parse_queryresult)
-	if answerset == qapair.answerset:
-		return True
+	if len(where) > 0:
+		where = where[0]
+		print graph
+		print where[1]
+		raw_answer = kb.query_where(where[1], return_vars="?u_" + str(where[0]), ask=ask_query)
+		answerset = AnswerSet(raw_answer, parser.parse_queryresult)
+		if answerset == qapair.answerset:
+			return True
 
 	return False
 
 if __name__ == "__main__":
-	bool_q = 0
+	multiple_answer_q = 0
 	count_q = 0
 	total = 0
 	no_answer = 0
-	type_q = 0
 	correct_answer = 0
 
-	ds = LC_Qaud_Linked(path="./data/LC-QUAD/linked_answer4.json")
-	kb = DBpedia()
-	# ds = WebQSP("./data/WebQuestionsSP/WebQSP.test.json")
-	# kb = Freebase()
+	# ds = LC_Qaud_Linked(path="./data/LC-QUAD/linked_answer4.json")
+	# kb = DBpedia()
+	# ds.load()
+	# ds.parse()
+
+	ds = WebQSP()
+	kb = Freebase()
+	ds.load()
+	ds.parse()
+	ds.extend("./data/WebQuestionsSP/WebQSP.test.json")
 
 	tmp = []
 	output = []
-	for qapair in prepare_dataset(ds).qapairs:
+	for qapair in ds.qapairs:
 		total += 1
 		print total
-		# if total <= 740:
+		# if total <= 28:
 		# 	continue
 		output_row = {}
 
 		output_row["question"] = qapair.question.text
 		output_row["id"] = qapair.id
 		output_row["no_answer"] = False
-		output_row["query"] = ""
+		output_row["query"] = qapair.sparql.query
 		output_row["correct_answer"] = False
 		if qapair.answerset is None or len(qapair.answerset) == 0:
 			no_answer += 1
 			output_row["no_answer"] = True
 		elif "COUNT(" in qapair.sparql.query:
 				count_q += 1
-				output_row["query"] = "COUNT"
-		# elif "ASK " in qapair.sparql.query:
-		# 		bool_q += 1
-		# 		output_row["query"] = "ASK"
+		elif qapair.answerset.number_of_answer() != 1:
+			multiple_answer_q += 1
 		else:
 			if qg(kb, ds.parser, qapair):
 				print "True"
@@ -97,14 +91,14 @@ if __name__ == "__main__":
 				print "False"
 			print "--"
 
-		# if total > 1:
+		# if total > 10:
 		# 	break
-		print no_answer, bool_q, count_q, type_q, correct_answer,  total
+		print no_answer, multiple_answer_q, count_q, correct_answer,  total
 		output.append(output_row)
 
 		if total % 100 == 0:
-			with open("output/6.json", "w") as data_file:
+			with open("output/wg_3.json", "w") as data_file:
 				json.dump(output, data_file, sort_keys=True, indent=4, separators=(',', ': '))
 
-	with open("output/tmp.6", "w") as data_file:
+	with open("output/wg_3.json", "w") as data_file:
 		json.dump(output, data_file, sort_keys=True, indent=4, separators=(',', ': '))
