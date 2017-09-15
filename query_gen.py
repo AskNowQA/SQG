b2f1044ceda5121aac8aded812d37676580e653b
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-15 09:57:53 +0200

    support ask queries

--- query_gen.py
+++ query_gen.py
@@ -1,5 +1,7 @@
 from parser.lc_quad_linked import LC_Qaud_Linked
+from parser.webqsp import WebQSP
 from kb.dbpedia import DBpedia
+from kb.freebase import Freebase
 from common.answerset import AnswerSet
 from common.graph.graph import Graph
 import json
@@ -10,13 +12,13 @@ def prepare_dataset(ds):
 	ds.parse()
 	return ds
 
-
-def qg(parser, qapair):
+def qg(kb, parser, qapair):
 	if qapair.answerset.number_of_answer() != 1:
 		return False
 
-	if "22-rdf-syntax-ns#type" in qapair.sparql.query:
-		pass
+
+	ask_query = "ASK " in qapair.sparql.query
+
 	print qapair.sparql
 	print qapair.question
 	all_uri = []
@@ -33,9 +35,8 @@ def qg(parser, qapair):
 	all_uri.extend(ontologies)
 	all_uri.extend(answer_uris)
 
-	kb = DBpedia()
 	graph = Graph(kb)
-	graph.find_minimal_subgraph(entities, ontologies, answer_uris)
+	graph.find_minimal_subgraph(entities, ontologies, answer_uris, ask_query)
 	print graph
 	print "-----"
 	where = graph.to_where_statement()
@@ -44,8 +45,8 @@ def qg(parser, qapair):
 	where = where[0]
 	print graph
 	print where
-	raw_answer = kb.query_where(where)
-	answerset = AnswerSet(raw_answer, parser.parse_answerset)
+	raw_answer = kb.query_where(where, ask=ask_query)
+	answerset = AnswerSet(raw_answer, parser.parse_queryresult)
 	if answerset == qapair.answerset:
 		return True
 
@@ -58,14 +59,18 @@ if __name__ == "__main__":
 	no_answer = 0
 	type_q = 0
 	correct_answer = 0
-	ds = LC_Qaud_Linked(path="./data/LC-QUAD/linked_answer4.json")
-	tmp = []
 
+	ds = LC_Qaud_Linked(path="./data/LC-QUAD/linked_answer4.json")
+	kb = DBpedia()
+	# ds = WebQSP("./data/WebQuestionsSP/WebQSP.test.json")
+	# kb = Freebase()
+
+	tmp = []
 	output = []
 	for qapair in prepare_dataset(ds).qapairs:
 		total += 1
 		print total
-		# if total <= 632:
+		# if total <= 740:
 		# 	continue
 		output_row = {}
 
@@ -80,31 +85,26 @@ if __name__ == "__main__":
 		elif "COUNT(" in qapair.sparql.query:
 				count_q += 1
 				output_row["query"] = "COUNT"
-		elif "ASK " in qapair.sparql.query:
-				bool_q += 1
-				output_row["query"] = "ASK"
-		# elif "22-rdf-syntax-ns#type" in qapair.sparql.query:
-		# 	type_q += 1
-		# 	output_row["query"] = "TYPE"
+		# elif "ASK " in qapair.sparql.query:
+		# 		bool_q += 1
+		# 		output_row["query"] = "ASK"
 		else:
-			if qg(ds.parser, qapair):
+			if qg(kb, ds.parser, qapair):
 				print "True"
 				correct_answer += 1
 				output_row["correct_answer"] = True
 			else:
 				print "False"
-			# print qapair.answerset
 			print "--"
 
-		# if total > 10:
+		# if total > 1:
 		# 	break
 		print no_answer, bool_q, count_q, type_q, correct_answer,  total
 		output.append(output_row)
 
 		if total % 100 == 0:
-			with open("output/5.json", "w") as data_file:
+			with open("output/6.json", "w") as data_file:
 				json.dump(output, data_file, sort_keys=True, indent=4, separators=(',', ': '))
 
-	with open("output/5.json", "w") as data_file:
+	with open("output/tmp.6", "w") as data_file:
 		json.dump(output, data_file, sort_keys=True, indent=4, separators=(',', ': '))
-
