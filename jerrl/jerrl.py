f37a7e0a87ee908b9217d7af05f423feb6187bc6
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-20 15:15:08 +0200

    dummy implementation for entity/relation linking

--- jerrl/jerrl.py
+++ jerrl/jerrl.py
@@ -7,20 +7,5 @@ class Jerrl:
 		pass
 
 	def do(self, qapair):
-		items = []
-		for raw_item in itertools.chain(qapair.raw_row["predicate mapping"], qapair.raw_row["entity mapping"]):
-			items.append(LinkedItem(raw_item, self.parse_linkeditem))
-		return items
-
-	def parse_linkeditem(self, raw_item):
-		return Uri(raw_item["uri"], self.parse_uri), raw_item["seq"], raw_item["label"]
-	
-	def parse_uri(self, raw_uri):
-		if raw_uri.find("/resource/") >= 0:
-			return "?s", raw_uri
-		elif raw_uri.find("/ontology/") >= 0 or raw_uri.find("/property/") >= 0:
-			return "?p", raw_uri
-		elif raw_uri.find("rdf-syntax-ns#type") >= 0:
-			return "?t", raw_uri
-		else:
-			return "?u", raw_uri
+		return set([u for u in qapair.sparql.uris if u.is_entity()]), \
+			   set([u for u in qapair.sparql.uris if u.is_ontology()])
