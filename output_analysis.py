41cb042f86ed438813e388cceb3a81dd27b1314c
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-22 12:15:08 +0200

    bug fix

--- output_analysis.py
+++ output_analysis.py
@@ -7,14 +7,17 @@ def load_ds(name):
 
 
 def diff(ds_1, ds_2, f1=None, f2=None):
+    result = []
     for i in range(len(ds_1)):
-
         if i < len(ds_2):
-            if ds_1[i] != ds_2[i]:
-                if (f1 is None or ds_1[i]["correct_answer"] == f1) and (f2 is None or ds_2[i]["correct_answer"] == f2):
-                    print ds_1[i]
-                    print ds_2[i]
-                    print
+            # if ds_1[i] != ds_2[i]:
+            if (f1 is None or ds_1[i]["correct_answer"] == f1) and (f2 is None or ds_2[i]["correct_answer"] == f2):
+                result.append(i)
+                print i
+                print ds_1[i]
+                print ds_2[i]
+                print
+    return result
 
 def default(ds, n=-1):
     total = 0
@@ -30,9 +33,25 @@ def default(ds, n=-1):
     pass
 
 if __name__ == "__main__":
+    print "LC_Quad"
     default(load_ds("1"))
     default(load_ds("2"))
     default(load_ds("3"))
     default(load_ds("4"))
-    default(load_ds("5"))
-    diff(load_ds("4"), load_ds("5"), True, False)
+    default(load_ds("5"))  # TYPE
+    default(load_ds("6"))  # BOOL
+    default(load_ds("7"))  # Bug fix
+    default(load_ds("8"))  # Two var-Fixed position
+    default(load_ds("9"))  # Fix dataset
+    default(load_ds("10"))  # Extend edge
+    default(load_ds("11"))  # Fix various bugs
+    default(load_ds("12"))  # Fix various bugs
+    result = diff(load_ds("11"), load_ds("12"), None, False)
+    print result
+
+    print "\nWebQuestion"
+    default(load_ds("wq_1"))
+    default(load_ds("wq_2"))  # BOOL
+    default(load_ds("wq_3"))  # Two var-Fixed position
+    default(load_ds("wq_5"))  # Fix various bugs
+    # diff(load_ds("wq_3"), load_ds("wq_5"), True, False)
