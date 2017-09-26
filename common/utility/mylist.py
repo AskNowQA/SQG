5e28b924e81ca8d943ffeea4ab6f4d09038bcfd3
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-26 11:30:02 +0200

    add less or equal method

--- common/utility/mylist.py
+++ common/utility/mylist.py
@@ -5,6 +5,12 @@ class MyList(list):
     def __init__(self, *args):
         super(MyList, self).__init__(*args)
 
+    def __le__(self, other):
+        l1 = Counter(self)
+        l2 = Counter(other)
+        counts = l1 - l2
+        return len(counts) == 0
+
     def __sub__(self, other):
         try:
             other_ = other
