96b35305a4df414505bd38cde102c54d8e7a088b
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-28 17:19:00 +0200

    filter out exisiting entities in the query from the answer

--- kb/freebase.py
+++ kb/freebase.py
@@ -1,4 +1,6 @@
 from kb import KB
+import re
+from itertools import chain
 
 
 class Freebase(KB):
@@ -6,6 +8,14 @@ class Freebase(KB):
 	def __init__(self, endpoint="http://drogon:9890/sparql"):
 		super(Freebase, self).__init__(endpoint)
 		self.type_uri = "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"
+
+	def query_where(self, clauses, return_vars="*", count=False, ask=False):
+		if not (count or ask) and return_vars != "*":
+			entities = list(chain.from_iterable([re.findall('ns:m.[a-z0-9]*', item) for item in clauses]))
+			return super(Freebase, self).query_where(
+				[u"FILTER ({} != {})".format(return_vars, entity) for entity in entities] + clauses,
+				return_vars=return_vars, count=count, ask=ask)
+		return super(Freebase, self).query_where(clauses, return_vars=return_vars, count=count, ask=ask)
 
 	@staticmethod
 	def shorten_prefix():
