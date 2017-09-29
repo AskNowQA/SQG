0dd011c6fb2f96a3eb243f395e80de613b73581c
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-29 15:20:21 +0200

    extract features from sparql query

--- common/sparql.py
+++ common/sparql.py
@@ -16,6 +16,35 @@ class SPARQL:
             self.where_clause = ' '.join(
                 self.where_clause[idx + len(WHERE) + 1:])
 
+    def query_features(self):
+        features = {"boolean": ["ask "],
+                    "count": ["count("],
+                    "filter": ["filter("],
+                    "comparison": ["<= ", ">= ", " < ", " > "],
+                    "sort": ["order by"],
+                    "aggregate": ["max(", "min("]
+                    }
+
+        output = set()
+        if self.where_clause.count(" ") > 3:
+            output.add("compound")
+        else:
+            output.add("single")
+        generic_uris = set()
+        for uri in self.uris:
+            if uri.is_generic():
+                generic_uris.add(uri)
+                if len(generic_uris) > 1:
+                    output.add("multivar")
+                    break
+        if len(generic_uris) <= 1:
+            output.add("singlevar")
+        raw_query = self.raw_query.lower()
+        for feature in features:
+            for constraint in features[feature]:
+                if constraint in raw_query:
+                    output.add(feature)
+        return output
 
     def __str__(self):
         return self.query.encode("ascii", "ignore")
