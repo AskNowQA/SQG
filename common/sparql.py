15915ea92792cf003900cbbabc38a55e1c823d5f
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-29 13:45:46 +0200

    bug fix

--- common/sparql.py
+++ common/sparql.py
@@ -1,20 +1,21 @@
 class SPARQL:
-	def __init__(self, raw_query, parser):
-		self.raw_query = raw_query
-		self.query, self.supported, self.uris = parser(raw_query)
-		self.extrat_where_template()
+    def __init__(self, raw_query, parser):
+        self.raw_query = raw_query
+        self.query, self.supported, self.uris = parser(raw_query)
+        self.extrat_where_template()
 
-	def extrat_where_template(self):
-		WHERE = "WHERE"
-		sparql_query = self.query.strip(" {};\t")
-		for uri in self.uris:
-			sparql_query = sparql_query.replace(uri.uri, uri.uri_type)
+    def extrat_where_template(self):
+        WHERE = "WHERE"
+        sparql_query = self.query.strip(" {};\t")
+        for uri in self.uris:
+            sparql_query = sparql_query.replace(uri.uri, uri.uri_type)
 
-		idx = sparql_query.find(WHERE)
-		self.where_clause = ' '.join(sparql_query.split())
-		if idx >= 0:
-			self.where_clause= ' '.join(self.where_clause[idx + len(WHERE) + 1:].strip("{}. ").replace(".", " ").split())
+        idx = sparql_query.find(WHERE)
+        self.where_clause = ' '.join(sparql_query.strip("{}. ").replace(".", " ").split())
+        if idx >= 0:
+            self.where_clause = ' '.join(
+                self.where_clause[idx + len(WHERE) + 1:])
 
 
-	def __str__(self):
-		return self.query.encode("ascii","ignore")
+    def __str__(self):
+        return self.query.encode("ascii", "ignore")
