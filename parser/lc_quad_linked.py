74006e4c6bdc43de168d0faa6974ebd8c3a9d409
hamidzafar <hamid.zafar.tud@gmail.com>
2017-09-29 13:48:33 +0200

    reformat code

--- parser/lc_quad_linked.py
+++ parser/lc_quad_linked.py
@@ -7,36 +7,38 @@ from answerparser import AnswerParser
 
 
 class LC_Qaud_Linked:
-	def __init__(self, path="./data/LC-QUAD/linked.json"):
-		self.raw_data = []
-		self.qapairs = []
-		self.path = path
-		self.parser = LC_Qaud_LinkedParser()
+    def __init__(self, path="./data/LC-QUAD/linked.json"):
+        self.raw_data = []
+        self.qapairs = []
+        self.path = path
+        self.parser = LC_Qaud_LinkedParser()
 
-	def load(self):
-		with open(self.path) as data_file:	
-			self.raw_data = json.load(data_file)
+    def load(self):
+        with open(self.path) as data_file:
+            self.raw_data = json.load(data_file)
 
-	def parse(self):		
-		for raw_row in self.raw_data:
-			self.qapairs.append(QApair(raw_row["question"], raw_row.get("answers"), raw_row["sparql_query"], raw_row, raw_row["id"], self.parser))
+    def parse(self):
+        for raw_row in self.raw_data:
+            self.qapairs.append(
+                QApair(raw_row["question"], raw_row.get("answers"), raw_row["sparql_query"], raw_row, raw_row["id"],
+                       self.parser))
 
-	def print_pairs(self, n=-1):
-		for item in self.qapairs[0:n]:
-			print item
-			print ""
+    def print_pairs(self, n=-1):
+        for item in self.qapairs[0:n]:
+            print item
+            print ""
 
 
 class LC_Qaud_LinkedParser(AnswerParser):
-	def __init__(self):
-		super(LC_Qaud_LinkedParser, self).__init__(DBpedia())
+    def __init__(self):
+        super(LC_Qaud_LinkedParser, self).__init__(DBpedia())
 
-	def parse_question(self, raw_question):
-		return raw_question
+    def parse_question(self, raw_question):
+        return raw_question
 
-	def parse_answerset(self, raw_answers):
-		return self.parse_queryresult(raw_answers)
+    def parse_answerset(self, raw_answers):
+        return self.parse_queryresult(raw_answers)
 
-	def parse_sparql(self, raw_query):
-		uris = [Uri(raw_uri, self.kb.parse_uri) for raw_uri in re.findall('<[^>]*>', raw_query)]
-		return raw_query, True, uris
+    def parse_sparql(self, raw_query):
+        uris = [Uri(raw_uri, self.kb.parse_uri) for raw_uri in re.findall('<[^>]*>', raw_query)]
+        return raw_query, True, uris
