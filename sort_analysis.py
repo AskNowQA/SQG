cecaa4bfb96bb58ddaacd74d10aa2a3ef6044e5d
hamidzafar <hamid.zafar.tud@gmail.com>
2017-10-17 11:33:25 +0200

    analysis sort questions in WebQSP

---
+++ sort_analysis.py
@@ -0,0 +1,40 @@
+from parser.webqsp import WebQSP
+from common.stats import Stats
+
+
+def prepare_dataset(ds):
+    ds.load()
+    ds.parse()
+    return ds
+
+
+if __name__ == "__main__":
+
+    stat = Stats()
+    # Qald(Qald.qald_6) LC_Qaud WebQSP
+    for item in prepare_dataset(WebQSP()).qapairs:
+        if "order by" in item.sparql.raw_query.lower():
+            stat.inc("total")
+            question = item.question.text.lower()
+            if " first" in question:
+                stat.inc("first")
+            elif " last " in question:
+                stat.inc("last")
+            elif "tallest" in question:
+                stat.inc("tallest")
+            elif " second " in question:
+                stat.inc("second")
+            elif "biggest" in question:
+                stat.inc("biggest")
+            elif " new " in question:
+                stat.inc("new")
+            elif " most " in question:
+                stat.inc("most")
+            else:
+                print item.question
+                print item.sparql.raw_query
+                print "-" * 10
+
+    print stat
+# biggest:4 first:29 last:20 most:5 new:2 second:1 tallest:1 total:109
+# biggest:4 first:62 most:11 new:46 second:2 tallest:1 total:3098
