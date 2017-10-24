7ad7fe8e7bbb315f624c0bdd093c9ce7c9bc694b
hamidzafar <hamid.zafar.tud@gmail.com>
2017-10-24 17:35:06 +0200

    first working version--at least no compile/runtime error

--- lsa/dssm.py
+++ lsa/dssm.py
@@ -1,22 +1,19 @@
-import pickle
 import random
 import time
-import sys
 import numpy as np
-import linecache
 import tensorflow as tf
 
 
-class DSSM2:
+class DSSM:
     def __init__(self):
         flags = tf.app.flags
         self.FLAGS = flags.FLAGS
 
         flags.DEFINE_string('summaries_dir', '/tmp/dssm-400-120-relu', 'Summaries directory')
         flags.DEFINE_float('learning_rate', 0.1, 'Initial learning rate.')
-        flags.DEFINE_integer('max_steps', 500, 'Number of steps to run trainer.')
+        flags.DEFINE_integer('max_steps', 1000, 'Number of steps to run trainer.')
         flags.DEFINE_integer('epoch_steps', 10, "Number of steps in one epoch.")
-        flags.DEFINE_integer('pack_size', 2000, "Number of batches in one pickle pack.")
+        flags.DEFINE_integer('pack_size', 20, "Number of batches in one pickle pack.")
         flags.DEFINE_bool('gpu', 1, "Enable GPU or not")
 
         self.VOCAB_SIZE = 50000  # VOCAB_SIZE
@@ -25,6 +22,21 @@ class DSSM2:
 
         self.L1_N = 400
         self.L2_N = 120
+
+        self.gamma = 20
+        self.model_path = "./tmp/models/dssm.model"
+
+    def __to_sparstensor2(self, input):
+        tmp = tf.not_equal(input, 0)
+        idx = tf.where(tmp)
+        values = tf.gather_nd(input, idx)
+        tns = tf.SparseTensor(idx, values, input.get_shape())
+        return tns
+
+    def __to_sparstensor(self, input):
+        idxs = np.where(input > 0)
+        return tf.SparseTensorValue(zip(idxs[0], idxs[1]), input[idxs],
+                                    [self.BS, self.VOCAB_SIZE])
 
     def data_iterator(self, train_set, sparse_output=False):
         index_start = 0
@@ -41,32 +53,33 @@ class DSSM2:
 
             index_start += self.BS
             if sparse_output:
-                idxs = np.where(questions_batch > 0)
-                questions_batch = tf.SparseTensorValue(zip(idxs[0], idxs[1]), questions_batch[idxs],
-                                                       [self.BS, self.VOCAB_SIZE])
-                idxs = np.where(sparqls_batch > 0)
-                sparqls_batch = tf.SparseTensorValue(zip(idxs[0], idxs[1]), sparqls_batch[idxs], [self.BS, self.VOCAB_SIZE])
+                questions_batch = self.__to_sparstensor(questions_batch)
+                sparqls_batch = self.__to_sparstensor(sparqls_batch)
 
             yield questions_batch, sparqls_batch, index_start
 
-    def train(self, train_set):
+    def __model(self):
         with tf.name_scope('input'):
-            query_batch = tf.sparse_placeholder(tf.float32, shape=None)
-            doc_batch = tf.sparse_placeholder(tf.float32, shape=None)
+            query_batch = tf.placeholder(tf.float32, shape=[self.BS, self.VOCAB_SIZE], name="query_batch")
+            doc_batch = tf.placeholder(tf.float32, shape=[self.BS, self.VOCAB_SIZE], name="doc_batch")
+            query_batch_sparse = self.__to_sparstensor2(query_batch)
+            doc_batch_sparse = self.__to_sparstensor2(doc_batch)
 
         with tf.name_scope('L1'):
             l1_par_range = np.sqrt(6.0 / (self.VOCAB_SIZE + self.L1_N))
-            weight1 = tf.Variable(tf.random_uniform([self.VOCAB_SIZE, self.L1_N], -l1_par_range, l1_par_range))
-            bias1 = tf.Variable(tf.random_uniform([self.L1_N], -l1_par_range, l1_par_range))
-            query_l1 = tf.sparse_tensor_dense_matmul(query_batch, weight1) + bias1
-            doc_l1 = tf.sparse_tensor_dense_matmul(doc_batch, weight1) + bias1
+            weight1 = tf.Variable(tf.random_uniform([self.VOCAB_SIZE, self.L1_N], -l1_par_range, l1_par_range),
+                                  name="weight1")
+            bias1 = tf.Variable(tf.random_uniform([self.L1_N], -l1_par_range, l1_par_range), name="bias1")
+            query_l1 = tf.sparse_tensor_dense_matmul(query_batch_sparse, weight1) + bias1
+            doc_l1 = tf.sparse_tensor_dense_matmul(doc_batch_sparse, weight1) + bias1
             query_l1_out = tf.nn.relu(query_l1)
             doc_l1_out = tf.nn.relu(doc_l1)
 
         with tf.name_scope('L2'):
             l2_par_range = np.sqrt(6.0 / (self.L1_N + self.L2_N))
-            weight2 = tf.Variable(tf.random_uniform([self.L1_N, self.L2_N], -l2_par_range, l2_par_range))
-            bias2 = tf.Variable(tf.random_uniform([self.L2_N], -l2_par_range, l2_par_range))
+            weight2 = tf.Variable(tf.random_uniform([self.L1_N, self.L2_N], -l2_par_range, l2_par_range),
+                                  name="weight2")
+            bias2 = tf.Variable(tf.random_uniform([self.L2_N], -l2_par_range, l2_par_range), name="bias2")
             query_l2 = tf.matmul(query_l1_out, weight2) + bias2
             doc_l2 = tf.matmul(doc_l1_out, weight2) + bias2
             query_y = tf.nn.relu(query_l2)
@@ -92,17 +105,21 @@ class DSSM2:
             norm_prod = tf.multiply(query_norm, doc_norm)
 
             cos_sim_raw = tf.truediv(prod, norm_prod)
-            cos_sim = tf.transpose(tf.reshape(tf.transpose(cos_sim_raw), [self.NEG + 1, self.BS])) * 20
+            cos_sim = tf.transpose(tf.reshape(tf.transpose(cos_sim_raw), [self.NEG + 1, self.BS])) * self.gamma
 
         with tf.name_scope('Loss'):
             # Train Loss
             prob = tf.nn.softmax((cos_sim))
             hit_prob = tf.slice(prob, [0, 0], [-1, 1])
-            loss = -tf.reduce_sum(tf.log(hit_prob)) / self.BS
+            # loss = -tf.reduce_sum(tf.log(hit_prob)) / self.BS
+            loss = tf.negative(tf.divide(tf.reduce_sum(tf.log(hit_prob)), self.BS), name="loss_op")
             tf.summary.scalar('loss', loss)
 
+        return loss, query_batch, doc_batch
+
+    def train(self, train_set):
+        loss, query_batch, doc_batch = self.__model()
         with tf.name_scope('Training'):
-            # Optimizer
             train_step = tf.train.GradientDescentOptimizer(self.FLAGS.learning_rate).minimize(loss)
 
         with tf.name_scope('Test'):
@@ -111,8 +128,8 @@ class DSSM2:
 
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
-        saver = tf.train.Saver()
-        iter_train = self.data_iterator(train_set, True)
+
+        iter_train = self.data_iterator(train_set, False)
         with tf.Session(config=config) as sess:
             sess.run(tf.global_variables_initializer())
             train_writer = tf.summary.FileWriter(self.FLAGS.summaries_dir + '/train', sess.graph)
@@ -133,13 +150,41 @@ class DSSM2:
 
                     print ("\nMiniBatch: %-5d | Train Loss: %-4.3f | PureTrainTime: %-3.3fs | File ptr: %d" %
                            (step, epoch_loss, end - start, index_start))
-            save_path = saver.save(sess, self.model_path)
+                    saver = tf.train.Saver()
+                    save_path = saver.save(sess, self.model_path, global_step=step)
+
+            saver = tf.train.Saver()
+            save_path = saver.save(sess, self.model_path, global_step=self.FLAGS.max_steps)
             print "Model saved in file: %s" % save_path
-
-            # for step in range(10):
-            #     query_in, doc_in, index_start = iter_test.next()
-            #     test_loss = sess.run(loss, feed_dict={query_batch: query_in, doc_batch: doc_in}) #, label_batch:label})
-            #     print ("\nMiniBatch: %-5d | Test Loss: %-4.3f" % (step, test_loss))
-
             end = time.time()
             print ("\nTotal time: %-3.3fs" % (end - start))
+
+    def test(self, test_set):
+        saver = tf.train.import_meta_graph(self.model_path + "-1000.meta")
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        iter_test = self.data_iterator(test_set, False)
+        with tf.Session(config=config) as sess:
+            ckpt = tf.train.get_checkpoint_state("./tmp/models/")
+            if ckpt and ckpt.model_checkpoint_path:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+
+            epoch_loss = 0
+            graph = tf.get_default_graph()
+            loss = graph.get_tensor_by_name("Loss/loss_op:0")
+            query_batch = graph.get_tensor_by_name("input/query_batch:0")
+            doc_batch = graph.get_tensor_by_name("input/doc_batch:0")
+            for i in range(self.FLAGS.pack_size):
+                query_in, doc_in, index_start = iter_test.next()
+                loss_v = sess.run(loss, feed_dict={query_batch: query_in, doc_batch: doc_in})
+                epoch_loss += loss_v
+
+            epoch_loss /= self.FLAGS.pack_size
+
+            print "Test Loss: %-4.3f" % epoch_loss
+
+
+if __name__ == "__main__":
+    q = DSSM()
+    # q.train1()
+    # q.test1()
