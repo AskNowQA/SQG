515fa6160ca138f2120612c64e7919af8dc0d010
hamidzafar <hamid.zafar.tud@gmail.com>
2017-10-25 16:43:33 +0200

    refactor

---
+++ common/preprocessing/preprocessor.py
@@ -0,0 +1,40 @@
+from jerrl.jerrl import Jerrl
+from common.preprocessing.wordhashing import WordHashing
+import numpy as np
+
+
+class Preprocessor:
+    @staticmethod
+    def qapair_to_hash(question_answer_uris):
+        jerrl = Jerrl()
+        hashing = WordHashing()
+        hashed_qapairs = {}
+        for data_item in question_answer_uris:
+            question = data_item["question"]
+            query = data_item["query"]
+            counter = 0
+            for item in jerrl.find_mentions(question, data_item["uris"]):
+                question = u"{0} {1} {2}".format(question[:item["start"]], str(counter), question[item["end"]:])
+                query = query.replace(item["uri"].raw_uri, str(counter))
+                counter += 1
+
+            hashed_question = hashing.hash(question)
+            hashed_sparql = hashing.hash(query)
+            hashed_qapairs[data_item["id"]] = (hashed_question, hashed_sparql)
+
+        VOCAB_SIZE = 50000  # len(hashing.ids)
+        DATA_SIZE = len(question_answer_uris)
+        i = 0
+        questions = np.zeros([DATA_SIZE, VOCAB_SIZE])
+        sparqls = np.zeros([DATA_SIZE, VOCAB_SIZE])
+        ids = []
+        for kv in hashed_qapairs.iteritems():
+            question_unique_counts = np.unique(kv[1][0], return_counts=True)
+            questions[i, question_unique_counts[0]] = question_unique_counts[1]
+
+            sparql_unique_counts = np.unique(kv[1][1], return_counts=True)
+            sparqls[i, sparql_unique_counts[0]] = sparql_unique_counts[1]
+            ids.append(kv[0])
+            i += 1
+
+        return questions, sparqls, ids
--- dssm.py
+++ dssm.py
@@ -1,58 +1,54 @@
-import numpy as np
 from sklearn.model_selection import train_test_split
-from common.preprocessing.wordhashing import WordHashing
 from parser.webqsp import WebQSP
 from parser.lc_quad import LC_Qaud
 from lsa.dssm import DSSM
+from common.preprocessing.preprocessor import Preprocessor
+from common.graph.graph import Graph
 from jerrl.jerrl import Jerrl
 
 
-def preprocessing(qapairs):
-    jerrl = Jerrl()
-    hashing = WordHashing()
-    hashed_qapairs = {}
-    for qapair in qapairs:
-        question = qapair.question.text
-        query = qapair.sparql.raw_query
-        counter = 0
-        for item in jerrl.mentions(qapair):
-            question = u"{0} {1} {2}".format(question[:item["start"]], str(counter), question[item["end"]:])
-            query = query.replace(item["uri"].raw_uri, str(counter))
-            counter += 1
-
-        hashed_question = hashing.hash(question)
-        hashed_sparql = hashing.hash(query)
-        hashed_qapairs[qapair.id] = (hashed_question, hashed_sparql)
-
-    VOCAB_SIZE = 50000  # len(hashing.ids)
-    DATA_SIZE = len(qapairs)
-    i = 0
-    questions = np.zeros([DATA_SIZE, VOCAB_SIZE])
-    sparqls = np.zeros([DATA_SIZE, VOCAB_SIZE])
-    for kv in hashed_qapairs.iteritems():
-        question_unique_counts = np.unique(kv[1][0], return_counts=True)
-        questions[i, question_unique_counts[0]] = question_unique_counts[1]
-
-        sparql_unique_counts = np.unique(kv[1][1], return_counts=True)
-        sparqls[i, sparql_unique_counts[0]] = sparql_unique_counts[1]
-        i += 1
-
-    return questions, sparqls
+def qapairs_to_triple(qapairs):
+    return [{"id": item.id, "question": item.question.text, "query": item.sparql.raw_query, "uris": item.sparql.uris}
+            for item in qapairs]
 
 
+jerrl = Jerrl()
 ds = LC_Qaud()
+kb = ds.parser.kb
 ds.load()
 ds.parse()
 
-ds_train, ds_test, _, _ = train_test_split(ds.qapairs, np.ones([len(ds.qapairs), 1]), test_size=0.2)
+# ds_train, ds_test, _, _ = train_test_split(ds.qapairs, [1] * len(ds.qapairs), test_size=0.2)
 
-# ds_train = ds.qapairs[:4000]
-# ds_test = ds.qapairs[4000:]
+ds_train = ds.qapairs[:4000]
+ds_test = ds.qapairs[4000:]
+
+ds_train = qapairs_to_triple(ds_train)
+ds_test = qapairs_to_triple(ds_test)
 
 model = DSSM(max_steps=10)
-# questions, sparqls = preprocessing(ds_train)
-# model.train([questions, sparqls])
-# questions, queries = preprocessing(ds_test)
+# questions, queries, ids = Preprocessor.qapair_to_hash(ds_train)
+# model.train([questions, queries])
+
+# questions, queries, ids = Preprocessor.qapair_to_hash(ds_test)
 # model.test([questions, queries])
-questions, sparqls = preprocessing(ds_test)
+
+new_ds_test = []
+for item in ds_test:
+    ask_query = "ASK " in item["query"]
+    count_query = "COUNT(" in item["query"]
+    sort_query = "order by" in item["query"].lower()
+
+    entities, ontologies = [u for u in item["uris"] if u.is_entity()], \
+                           [u for u in item["uris"] if u.is_ontology()]
+
+    graph = Graph(kb)
+    graph.find_minimal_subgraph(entities, ontologies, ask_query, sort_query)
+    where = graph.to_where_statement()
+    if len(where) > 1:
+        for w in where:
+            new_ds_test.append({"id": item["id"], "question": item["question"], "query": " ".join(w[1]),
+                                "uris": item["uris"]})
+
+questions, sparqls = Preprocessor.qapair_to_hash(new_ds_test)
 model.similarity(questions, sparqls)
