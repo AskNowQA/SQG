bf333a0dd97ca4ae690a2f968575e8346ba9f64c
hamidzafar <hamid.zafar.tud@gmail.com>
2017-11-15 17:14:36 +0100

    refactor

--- linker/jerrl.py
+++ linker/jerrl.py
@@ -1,6 +1,5 @@
-import itertools
 from common.container.linkeditem import LinkedItem
-from common.container.uri import Uri
+from common.utility.utility import find_mentions
 
 
 class Jerrl:
@@ -12,7 +11,7 @@ class Jerrl:
         relations = []
         for u in qapair.sparql.uris:
             question = qapair.question.text
-            mentions = self.find_mentions(question, [u])
+            mentions = find_mentions(question, [u])
             surface = ""
             if len(mentions) > 0:
                 surface = question[mentions[0]["start"]:mentions[0]["end"]]
@@ -24,104 +23,3 @@ class Jerrl:
                 relations.append(linked_item)
 
         return entities, relations
-
-    def find_mentions(self, text, uris):
-        output = []
-        for uri in uris:
-            s, e, dist = Jerrl.__substring_with_min_levenshtein_distance(str(uri), text)
-            if dist <= 5:
-                output.append({"uri": uri, "start": s, "end": e})
-        return output
-
-    @staticmethod
-    def __fuzzy_substring(needle, haystack):
-        """Calculates the fuzzy match of needle in haystack,
-        using a modified version of the Levenshtein distance
-        algorithm.
-        The function is modified from the levenshtein function
-        in the bktree module by Adam Hupp"""
-        m, n = len(needle), len(haystack)
-
-        # base cases
-        if m == 1:
-            # return not needle in haystack
-            row = [len(haystack)] * len(haystack)
-            row[haystack.find(needle)] = 0
-            return row
-        if not n:
-            return m
-
-        row1 = [0] * (n + 1)
-        for i in range(0, m):
-            row2 = [i + 1]
-            for j in range(0, n):
-                cost = (needle[i] != haystack[j])
-
-                row2.append(min(row1[j + 1] + 1,  # deletion
-                                row2[j] + 1,  # insertion
-                                row1[j] + cost)  # substitution
-                            )
-            row1 = row2
-        return row1
-
-    @staticmethod
-    def __min_farest(values):
-        return -min((x, -i) for i, x in enumerate(values))[1]
-
-    @staticmethod
-    def __min_nearest(values):
-        return min(enumerate(values), key=lambda p: p[1])[0]
-
-    @staticmethod
-    def __levenshtein(s1, s2):
-        if len(s1) < len(s2):
-            return Jerrl.__levenshtein(s2, s1)
-
-        # len(s1) >= len(s2)
-        if len(s2) == 0:
-            return len(s1)
-
-        previous_row = range(len(s2) + 1)
-        for i, c1 in enumerate(s1):
-            current_row = [i + 1]
-            for j, c2 in enumerate(s2):
-                insertions = previous_row[
-                                 j + 1] + 1  # j+1 instead of j since previous_row and current_row are one character longer
-                deletions = current_row[j] + 1  # than s2
-                substitutions = previous_row[j] + (c1 != c2)
-                current_row.append(min(insertions, deletions, substitutions))
-            previous_row = current_row
-
-        return previous_row[-1]
-
-    @staticmethod
-    def __substring_with_min_levenshtein_distance(n, h):
-        n = n.lower().replace("_", " ")
-        h = h.lower()
-        row = Jerrl.__fuzzy_substring(n, h)
-        end = min(Jerrl.__min_farest(row), len(h) - 1)
-        row_rev = Jerrl.__fuzzy_substring(n[::-1], h[::-1])
-        start = max(0, len(h) - Jerrl.__min_nearest(row_rev) - 1)
-
-        strip = [" ", "?", ".", ",", "'"]
-        # stretch the token to be whole word[s]
-        while h[start] not in strip and start >= 0:
-            start -= 1
-
-        while h[end - 1] not in strip and end < (len(h) - 1):
-            end += 1
-
-        # remove invalid chars in head or tail
-        for i in range(start, end):
-            if h[start] in strip:
-                start += 1
-            else:
-                break
-
-        for i in range(end, start, -1):
-            if h[end - 1] in strip:
-                end -= 1
-            else:
-                break
-
-        return start, end, row[end]
