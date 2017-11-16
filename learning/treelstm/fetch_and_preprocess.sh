08498799dd6f083594569dbce2813a16a2d47b12
hamidzafar <hamid.zafar.tud@gmail.com>
2017-11-16 12:19:51 +0100

    update scripts name

--- learning/treelstm/fetch_and_preprocess.sh
+++ learning/treelstm/fetch_and_preprocess.sh
@@ -4,4 +4,4 @@ python2.7 scripts/download.py
 
 CLASSPATH="lib:lib/stanford-parser/stanford-parser.jar:lib/stanford-parser/stanford-parser-3.5.1-models.jar"
 javac -cp $CLASSPATH lib/*.java
-python2.7 scripts/preprocess-sick.py
+python2.7 scripts/preprocess-lcquad.py
