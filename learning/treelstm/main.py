227639d7f7ffd206590e507f0b558596ce98e614
hamidzafar <hamid.zafar.tud@gmail.com>
2017-11-16 12:21:04 +0100

    load test and dev sets

--- learning/treelstm/main.py
+++ learning/treelstm/main.py
@@ -80,7 +80,7 @@ def main():
                   data=[Constants.PAD_WORD, Constants.UNK_WORD, Constants.BOS_WORD, Constants.EOS_WORD])
     logger.debug('==> LC-Quad vocabulary size : %d ' % vocab.size())
 
-    # load SICK dataset splits
+    # load LC_quad dataset splits
     train_file = os.path.join(args.data, 'lcquad_train.pth')
     if os.path.isfile(train_file):
         train_dataset = torch.load(train_file)
@@ -88,20 +88,20 @@ def main():
         train_dataset = LCQuadDataset(train_dir, vocab, args.num_classes)
         torch.save(train_dataset, train_file)
     logger.debug('==> Size of train data   : %d ' % len(train_dataset))
-    # dev_file = os.path.join(args.data, 'lcquad_dev.pth')
-    # if os.path.isfile(dev_file):
-    #     dev_dataset = torch.load(dev_file)
-    # else:
-    #     dev_dataset = LCQuadDataset(dev_dir, vocab, args.num_classes)
-    #     torch.save(dev_dataset, dev_file)
-    # logger.debug('==> Size of dev data     : %d ' % len(dev_dataset))
-    # test_file = os.path.join(args.data, 'lcquad_test.pth')
-    # if os.path.isfile(test_file):
-    #     test_dataset = torch.load(test_file)
-    # else:
-    #     test_dataset = LCQuadDataset(test_dir, vocab, args.num_classes)
-    #     torch.save(test_dataset, test_file)
-    # logger.debug('==> Size of test data    : %d ' % len(test_dataset))
+    dev_file = os.path.join(args.data, 'lcquad_dev.pth')
+    if os.path.isfile(dev_file):
+        dev_dataset = torch.load(dev_file)
+    else:
+        dev_dataset = LCQuadDataset(dev_dir, vocab, args.num_classes)
+        torch.save(dev_dataset, dev_file)
+    logger.debug('==> Size of dev data     : %d ' % len(dev_dataset))
+    test_file = os.path.join(args.data, 'lcquad_test.pth')
+    if os.path.isfile(test_file):
+        test_dataset = torch.load(test_file)
+    else:
+        test_dataset = LCQuadDataset(test_dir, vocab, args.num_classes)
+        torch.save(test_dataset, test_file)
+    logger.debug('==> Size of test data    : %d ' % len(test_dataset))
 
     # initialize model, criterion/loss_function, optimizer
     model = SimilarityTreeLSTM(
@@ -111,7 +111,7 @@ def main():
         args.hidden_dim,
         args.num_classes,
         args.sparse)
-    criterion = nn.KLDivLoss()
+    criterion = nn.HingeEmbeddingLoss()  # nn.KLDivLoss()
     if args.cuda:
         model.cuda(), criterion.cuda()
     if args.optim == 'adam':
@@ -151,28 +151,28 @@ def main():
     for epoch in range(args.epochs):
         train_loss = trainer.train(train_dataset)
         train_loss, train_pred = trainer.test(train_dataset)
-        # dev_loss, dev_pred = trainer.test(dev_dataset)
-        # test_loss, test_pred = trainer.test(test_dataset)
+        dev_loss, dev_pred = trainer.test(dev_dataset)
+        test_loss, test_pred = trainer.test(test_dataset)
 
         train_pearson = metrics.pearson(train_pred, train_dataset.labels)
         train_mse = metrics.mse(train_pred, train_dataset.labels)
         logger.info(
             '==> Epoch {}, Train \tLoss: {}\tPearson: {}\tMSE: {}'.format(epoch, train_loss, train_pearson, train_mse))
-        # dev_pearson = metrics.pearson(dev_pred, dev_dataset.labels)
-        # dev_mse = metrics.mse(dev_pred, dev_dataset.labels)
-        # logger.info('==> Epoch {}, Dev \tLoss: {}\tPearson: {}\tMSE: {}'.format(epoch, dev_loss, dev_pearson, dev_mse))
-        # test_pearson = metrics.pearson(test_pred, test_dataset.labels)
-        # test_mse = metrics.mse(test_pred, test_dataset.labels)
-        # logger.info(
-        #     '==> Epoch {}, Test \tLoss: {}\tPearson: {}\tMSE: {}'.format(epoch, test_loss, test_pearson, test_mse))
+        dev_pearson = metrics.pearson(dev_pred, dev_dataset.labels)
+        dev_mse = metrics.mse(dev_pred, dev_dataset.labels)
+        logger.info('==> Epoch {}, Dev \tLoss: {}\tPearson: {}\tMSE: {}'.format(epoch, dev_loss, dev_pearson, dev_mse))
+        test_pearson = metrics.pearson(test_pred, test_dataset.labels)
+        test_mse = metrics.mse(test_pred, test_dataset.labels)
+        logger.info(
+            '==> Epoch {}, Test \tLoss: {}\tPearson: {}\tMSE: {}'.format(epoch, test_loss, test_pearson, test_mse))
 
-        # if best < test_pearson:
-        #     best = test_pearson
-        #     checkpoint = {'model': trainer.model.state_dict(), 'optim': trainer.optimizer,
-        #                   'pearson': test_pearson, 'mse': test_mse,
-        #                   'args': args, 'epoch': epoch}
-        #     logger.debug('==> New optimum found, checkpointing everything now...')
-        #     torch.save(checkpoint, '%s.pt' % os.path.join(args.save, args.expname))
+        if best < test_pearson:
+            best = test_pearson
+        checkpoint = {'model': trainer.model.state_dict(), 'optim': trainer.optimizer,
+                      'pearson': test_pearson, 'mse': test_mse,
+                      'args': args, 'epoch': epoch}
+        logger.debug('==> New optimum found, checkpointing everything now...')
+        torch.save(checkpoint, '%s.pt' % os.path.join(args.save, args.expname))
 
 
 if __name__ == "__main__":
