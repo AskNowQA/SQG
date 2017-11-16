362bcc38365661ec3a58bc4bb245620744a4a03d
hamidzafar <hamid.zafar.tud@gmail.com>
2017-11-16 12:21:28 +0100

    reformate

--- learning/treelstm/model.py
+++ learning/treelstm/model.py
@@ -5,28 +5,29 @@ from torch.autograd import Variable as Var
 
 import Constants
 
+
 # module for childsumtreelstm
 class ChildSumTreeLSTM(nn.Module):
     def __init__(self, in_dim, mem_dim):
         super(ChildSumTreeLSTM, self).__init__()
         self.in_dim = in_dim
         self.mem_dim = mem_dim
-        self.ioux = nn.Linear(self.in_dim, 3*self.mem_dim)
-        self.iouh = nn.Linear(self.mem_dim, 3*self.mem_dim)
-        self.fx = nn.Linear(self.in_dim,self.mem_dim)
-        self.fh = nn.Linear(self.mem_dim,self.mem_dim)
+        self.ioux = nn.Linear(self.in_dim, 3 * self.mem_dim)
+        self.iouh = nn.Linear(self.mem_dim, 3 * self.mem_dim)
+        self.fx = nn.Linear(self.in_dim, self.mem_dim)
+        self.fh = nn.Linear(self.mem_dim, self.mem_dim)
 
     def node_forward(self, inputs, child_c, child_h):
         child_h_sum = torch.sum(child_h, dim=0, keepdim=True)
 
         iou = self.ioux(inputs) + self.iouh(child_h_sum)
-        i, o, u = torch.split(iou, iou.size(1)//3, dim=1)
+        i, o, u = torch.split(iou, iou.size(1) // 3, dim=1)
         i, o, u = F.sigmoid(i), F.sigmoid(o), F.tanh(u)
 
         f = F.sigmoid(
-                self.fh(child_h) +
-                self.fx(inputs).repeat(len(child_h), 1)
-            )
+            self.fh(child_h) +
+            self.fx(inputs).repeat(len(child_h), 1)
+        )
         fc = torch.mul(f, child_c)
 
         c = torch.mul(i, u) + torch.sum(fc, dim=0, keepdim=True)
@@ -36,15 +37,16 @@ class ChildSumTreeLSTM(nn.Module):
     def forward(self, tree, inputs):
         _ = [self.forward(tree.children[idx], inputs) for idx in range(tree.num_children)]
 
-        if tree.num_children==0:
+        if tree.num_children == 0:
             child_c = Var(inputs[0].data.new(1, self.mem_dim).fill_(0.))
             child_h = Var(inputs[0].data.new(1, self.mem_dim).fill_(0.))
         else:
-            child_c, child_h = zip(* map(lambda x: x.state, tree.children))
+            child_c, child_h = zip(*map(lambda x: x.state, tree.children))
             child_c, child_h = torch.cat(child_c, dim=0), torch.cat(child_h, dim=0)
 
         tree.state = self.node_forward(inputs[tree.idx], child_c, child_h)
         return tree.state
+
 
 # module for distance-angle similarity
 class Similarity(nn.Module):
@@ -53,7 +55,7 @@ class Similarity(nn.Module):
         self.mem_dim = mem_dim
         self.hidden_dim = hidden_dim
         self.num_classes = num_classes
-        self.wh = nn.Linear(2*self.mem_dim, self.hidden_dim)
+        self.wh = nn.Linear(2 * self.mem_dim, self.hidden_dim)
         self.wp = nn.Linear(self.hidden_dim, self.num_classes)
 
     def forward(self, lvec, rvec):
@@ -64,6 +66,7 @@ class Similarity(nn.Module):
         out = F.sigmoid(self.wh(vec_dist))
         out = F.log_softmax(self.wp(out))
         return out
+
 
 # putting the whole model together
 class SimilarityTreeLSTM(nn.Module):
