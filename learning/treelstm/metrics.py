fe64b9ee1fbe0ad16bb2ca5d2af2be5d46cf871e
hamidzafar <hamid.zafar.tud@gmail.com>
2017-11-20 14:25:25 +0100

    change range of lables to 1-2

--- learning/treelstm/metrics.py
+++ learning/treelstm/metrics.py
@@ -13,19 +13,22 @@ class Metrics():
                                                          self.f1(predictions, labels))
 
     def f1(self, predictions, labels):
-        true = labels == predictions
-        false = labels != predictions
-        assert torch.sum(true) + torch.sum(false) == len(predictions)
-        true_positive = torch.sum(true[labels == 1])
-        true_negative = torch.sum(true[labels == -1])
+        try:
+            true = labels == predictions
+            false = labels != predictions
+            assert torch.sum(true) + torch.sum(false) == len(predictions)
+            true_positive = torch.sum(true[labels == 1])
+            true_negative = torch.sum(true[labels == -1])
 
-        false_positive = torch.sum(false[labels == 1])
-        false_negative = torch.sum(false[labels == -1])
+            false_positive = torch.sum(false[labels == 1])
+            false_negative = torch.sum(false[labels == -1])
 
-        precision = 1.0 * true_positive / (true_positive + false_positive)
-        recall = 1.0 * true_positive / (true_positive + false_negative)
-        f1 = 2.0 * (precision * recall) / (precision + recall)
-        return precision, recall, f1
+            precision = 1.0 * true_positive / (true_positive + false_positive)
+            recall = 1.0 * true_positive / (true_positive + false_negative)
+            f1 = 2.0 * (precision * recall) / (precision + recall)
+            return precision, recall, f1
+        except:
+            return 0, 0, 0
 
     def pearson(self, predictions, labels):
         x = deepcopy(predictions)
