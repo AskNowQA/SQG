4a1e7b0616cff14a49c5dd1c4130f310a71e52a5
hamidzafar <hamid.zafar.tud@gmail.com>
2017-11-20 14:25:46 +0100

    revert to original version

--- learning/treelstm/utils.py
+++ learning/treelstm/utils.py
@@ -60,15 +60,15 @@ def build_vocab(filenames, vocabfile):
 # mapping from scalar to vector
 def map_label_to_target(label, num_classes):
     target = torch.zeros(1, num_classes)
-    if label == -1:
-        target[0][0] = 1
-    else:
-        target[0][1] = 1
-    # ceil = int(math.ceil(label))
-    # floor = int(math.floor(label))
-    # if ceil == floor:
-    #     target[0][floor - 1] = 1
+    # if label == -1:
+    #     target[0][0] = 1
     # else:
-    #     target[0][floor - 1] = ceil - label
-    #     target[0][ceil - 1] = label - floor
+    #     target[0][1] = 1
+    ceil = int(math.ceil(label))
+    floor = int(math.floor(label))
+    if ceil == floor:
+        target[0][floor - 1] = 1
+    else:
+        target[0][floor - 1] = ceil - label
+        target[0][ceil - 1] = label - floor
     return target
