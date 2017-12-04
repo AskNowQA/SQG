b0154d80469e9cf7bbb1c35ccfe966e67c1e719f
hamidzafar <hamid.zafar.tud@gmail.com>
2017-12-04 16:23:28 +0100

    refactor

--- learning/cdssm/dataset.py
+++ learning/cdssm/dataset.py
@@ -1,21 +1,22 @@
-import os
-from tqdm import tqdm
 from copy import deepcopy
-import torch
 import torch.utils.data as data
 
 
 class QGDataset(data.Dataset):
-    def __init__(self, path, vocab, num_classes):
+    def __init__(self, num_classes, vocab_size):
         super(QGDataset, self).__init__()
-        self.vocab = vocab
         self.num_classes = num_classes
 
-        self.questions = self.read_hashes(os.path.join(path, 'hashed_questions.txt'))
-        self.queries = self.read_hashes(os.path.join(path, 'hashed_queries.txt'))
+        self.questions = []
+        self.queries = []
+        self.labels = []
+        self.size = 0
+        self.vocab_size = vocab_size
 
-        self.labels = self.read_labels(os.path.join(path, 'sim.txt'))
-
+    def add(self, question, query, label):
+        self.questions.append(question)
+        self.queries.append(query)
+        self.labels.append(label)
         self.size = len(self.questions)
 
     def __len__(self):
@@ -26,18 +27,3 @@ class QGDataset(data.Dataset):
         query = deepcopy(self.queries[index])
         label = deepcopy(self.labels[index])
         return question, query, label
-
-    def read_hashes(self, filename):
-        with open(filename, 'r') as f:
-            sentences = [self.read_hash(line) for line in tqdm(f.readlines())]
-        return sentences
-
-    def read_hash(self, line):
-        indices = map(int, line.split())
-        return torch.LongTensor(indices)
-
-    def read_labels(self, filename):
-        with open(filename, 'r') as f:
-            labels = list(map(lambda x: float(x), f.readlines()))
-            labels = torch.Tensor(labels)
-        return labels
