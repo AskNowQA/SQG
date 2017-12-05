7fa1a9f030e48a21852d1497e68d4fd61501124f
hamidzafar <hamid.zafar.tud@gmail.com>
2017-12-05 15:38:19 +0100

    sparse_cat added

--- learning/cdssm/utils.py
+++ learning/cdssm/utils.py
@@ -1,7 +1,9 @@
 from __future__ import division
 from __future__ import print_function
+from copy import deepcopy as copy
 import math
 import torch
+import numpy as np
 
 
 # mapping from scalar to vector
@@ -19,3 +21,18 @@ def map_label_to_target(label, num_classes):
         target[0][floor - 1] = ceil - label
         target[0][ceil - 1] = label - floor
     return target
+
+
+def accumu(l):
+    total = 0
+    for x in l:
+        total += x
+        yield total
+
+
+def sparse_cat(l, size):
+    output = l[0]
+    for i in range(1, len(l)):
+        output = np.concatenate([output, np.array(zip(l[i][:, 0] + (size * i), l[i][:, 1]))])
+
+    return np.array(output)
