a95da4a23b5c3b4fe026f66a568a011f2dcc5ae6
hamidzafar <hamid.zafar.tud@gmail.com>
2018-01-12 21:25:43 +0100

    class name resolve

--- analysis/earl.py
+++ analysis/earl.py
@@ -1,6 +1,6 @@
 from parser.lc_quad_linked import LC_Qaud_Linked
 from linker.earl import Earl
-from linker.jerrl import Jerrl
+from linker.goldLinker import GoldLinker
 from common.utility.stats import Stats
 from tqdm import tqdm
 
@@ -28,11 +28,11 @@ if __name__ == "__main__":
     ds.load()
     ds.parse()
 
-    jerrl = Jerrl()
+    goldLinker = GoldLinker()
     earl = Earl("../data/LC-QUAD/EARL/output.json")
 
     for qapair in tqdm(ds.qapairs):
-        e1, r1 = jerrl.do(qapair)
+        e1, r1 = goldLinker.do(qapair)
         e2, r2 = earl.do(qapair, force_gold=False, top=100)
         if len(e2) == 0:
             stats.inc("earl_no_entity")
