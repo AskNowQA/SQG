fe8f912296c167cf5e1245f3445a1a286ec5b952
hamidzafar <hamid.zafar.tud@gmail.com>
2018-02-07 17:13:52 +0100

    add progress bar

--- scripts/generate_bloom.py
+++ scripts/generate_bloom.py
@@ -1,6 +1,9 @@
 from pybloom import BloomFilter
 import mmap
 import argparse
+from tqdm import tqdm
+import subprocess
+import os
 
 if __name__ == "__main__":
 
@@ -10,15 +13,26 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.file != "":
-        bloom = BloomFilter(capacity=200000000, error_rate=0.000001)
+        bloom = BloomFilter(capacity=400000000, error_rate=0.000001)
 
+        lines_in_file = int(subprocess.check_output("wc -l " + args.file, shell=True).split()[0])
         with open(args.file) as infile:
             m = mmap.mmap(infile.fileno(), 0, access=mmap.ACCESS_READ)
-            for line in iter(m.readline, ""):
-                s, p, o = line.split(",")
-                bloom.add(s + ":" + p)
-                bloom.add(p + ":" + o)
-            m.close()
+            for line in tqdm(iter(m.readline, ""), total=lines_in_file):
 
-        with open(args.output, "w") as outfile:
-            bloom.tofile(outfile)
+                tmp = line.replace("\n", "").split(",http")
+                for i in range(1, len(tmp)):
+                    tmp[i] = "http" + tmp[i]
+                if len(tmp) != 3:
+                    # print tmp
+                    continue
+
+                s, p, o = tmp
+                s1 = s + ":" + p
+                s2 = p + ":" + o
+                bloom.add(s1)
+                bloom.add(s2)
+        m.close()
+
+    with open(args.output, "w") as outfile:
+        bloom.tofile(outfile)
