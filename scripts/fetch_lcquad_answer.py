b8ced353c281fefe460ad25f9e3065b76a5aff4c
hamidzafar <hamid.zafar.tud@gmail.com>
2018-03-06 11:35:18 +0100

    reformat

--- scripts/fetch_lcquad_answer.py
+++ scripts/fetch_lcquad_answer.py
@@ -4,87 +4,88 @@ from parser.lc_quad import LC_Qaud
 
 
 def prepare_dataset(ds):
-	ds.load()
-	ds.parse()
-	return ds
+    ds.load()
+    ds.parse()
+    return ds
+
 
 def ask_query(uri):
-	if uri == "<https://www.w3.org/1999/02/22-rdf-syntax-ns#type>":
-		return 200, json.loads("{\"boolean\": \"True\"}")
-	uri = uri.replace("https://","http://")
-	return query(u'ASK WHERE {{ {} ?u ?x }}'.format(uri))
+    if uri == "<https://www.w3.org/1999/02/22-rdf-syntax-ns#type>":
+        return 200, json.loads("{\"boolean\": \"True\"}")
+    uri = uri.replace("https://", "http://")
+    return query(u'ASK WHERE {{ {} ?u ?x }}'.format(uri))
+
 
 def query(q):
-	q = q.replace("https://","http://")
-	payload = (
-		# ('default-graph-uri', 'http://kb.org'),
-		('query', q),
-		('format', 'application/json'))
+    q = q.replace("https://", "http://")
+    payload = (
+        # ('default-graph-uri', 'http://kb.org'),
+        ('query', q),
+        ('format', 'application/json'))
 
-	print q
-	r = requests.get('http://drogon:7890/sparql', params=payload)
-	return r.status_code, r.json()
+    print q
+    r = requests.get('http://drogon:7890/sparql', params=payload)
+    return r.status_code, r.json()
 
 
 def has_answer(t):
-	if "results" in t and len(t["results"]["bindings"]) > 0:
-		return True
-	if "boolean" in t:
-		return True
-	return False
+    if "results" in t and len(t["results"]["bindings"]) > 0:
+        return True
+    if "boolean" in t:
+        return True
+    return False
 
 
 if __name__ == "__main__":
-	# print query("SELECT DISTINCT ?uri WHERE {?uri <http://dbpedia.org/ontology/developer> <http://dbpedia.org/resource/J._Michael_Straczynski> . ?uri <http://dbpedia.org/property/network> <http://dbpedia.org/resource/TNT_(TV_channel)>  . ?uri <https://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/TelevisionShow>}".replace("/property", "/ontology"))
-	i=0
-	# ds = LC_Qaud_Linked(path="./data/LC-QUAD/linked.json")
-	ds = LC_Qaud()
-	tmp = []
-	no_answer = 0
-	no_entity = 0
-	for qapair in prepare_dataset(ds).qapairs:
-		raw_row = dict()
-		raw_row["id"] = qapair.id.__str__()
-		raw_row["question"] = qapair.question.__str__()
-		raw_row["sparql_query"] = qapair.sparql.query
-		try:
-			r = query(qapair.sparql.query)
-			raw_row["answers"] = r[1]
-		except Exception as e:
-			raw_row["answers"] = []
-			print e
-			print
+    # print query("SELECT DISTINCT ?uri WHERE {?uri <http://dbpedia.org/ontology/developer> <http://dbpedia.org/resource/J._Michael_Straczynski> . ?uri <http://dbpedia.org/property/network> <http://dbpedia.org/resource/TNT_(TV_channel)>  . ?uri <https://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/TelevisionShow>}".replace("/property", "/ontology"))
+    i = 0
+    # ds = LC_Qaud_Linked(path="./data/LC-QUAD/linked.json")
+    ds = LC_Qaud()
+    tmp = []
+    no_answer = 0
+    no_entity = 0
+    for qapair in prepare_dataset(ds).qapairs:
+        raw_row = dict()
+        raw_row["id"] = qapair.id.__str__()
+        raw_row["question"] = qapair.question.__str__()
+        raw_row["sparql_query"] = qapair.sparql.query
+        try:
+            r = query(qapair.sparql.query)
+            raw_row["answers"] = r[1]
+        except Exception as e:
+            raw_row["answers"] = []
+            print e
+            print
 
-		if not has_answer(raw_row["answers"]):
-		# 	print qapair.question
-		# 	print qapair.raw_row["answers"]
-		# 	print qapair.sparql.query
-		# 	print
-			i+=1
-			# try:
-			# 	r = query(qapair.sparql.query.replace("/property", "/ontology"))
-			# 	qapair.raw_row["answers"] = r[1]
-			# 	if has_answer(r[1]):
-			# 		print r[1]
-			# 		print qapair.raw_row["answers"]
-			# 		print qapair.sparql.query
-			# 		print 
-			# except Exception as e:
-			# 	qapair.raw_row["answers"] = []
-			# 	print e
-			# 	print
+        if not has_answer(raw_row["answers"]):
+            # 	print qapair.question
+            # 	print qapair.raw_row["answers"]
+            # 	print qapair.sparql.query
+            # 	print
+            i += 1
+        # try:
+        # 	r = query(qapair.sparql.query.replace("/property", "/ontology"))
+        # 	qapair.raw_row["answers"] = r[1]
+        # 	if has_answer(r[1]):
+        # 		print r[1]
+        # 		print qapair.raw_row["answers"]
+        # 		print qapair.sparql.query
+        # 		print
+        # except Exception as e:
+        # 	qapair.raw_row["answers"] = []
+        # 	print e
+        # 	print
 
-		
-		# for uri in qapair.sparql.uris:
-		# 	if not ask_query(uri.uri)[1]["boolean"]:
-		# 		no_entity += 1
-		
-		
-		# if i > 10:
-		# 	break
-		print i
-		tmp.append(raw_row)
-	print i
+        # for uri in qapair.sparql.uris:
+        # 	if not ask_query(uri.uri)[1]["boolean"]:
+        # 		no_entity += 1
 
-	with open('data/LC-QUAD/linked_answer5.json', 'w') as jsonFile:
-		json.dump(tmp, jsonFile)
+
+        # if i > 10:
+        # 	break
+        print i
+        tmp.append(raw_row)
+    print i
+
+    with open('data/LC-QUAD/linked_answer5.json', 'w') as jsonFile:
+        json.dump(tmp, jsonFile)
