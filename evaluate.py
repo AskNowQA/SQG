cd1b7bd36447f29f15b0dd92658c174331ac6d1b
just3obad <just3obad@gmail.com>
2018-04-25 17:43:24 +0200

    Qald 765 analysis

--- evaluate.py
+++ evaluate.py
@@ -10,9 +10,11 @@ import glob
 import fnmatch
 import sys
 
+
 def load_file(name):
-    with open("output/{}.json".format(name)) as data_file:
+    with open("output/{}".format(name)) as data_file:
         return json.load(data_file)
+
 
 # takes the query_gen output file format not the analysis_deep format
 def basic_stats(data):
@@ -50,34 +52,74 @@ def basic_stats(data):
     print "AA-  F1: %.2f" % f1
     return correct, incorrect, no_path, no_answer
 
-# Get incorrect data and saves them in a json file externally
-def deep_analysis(data, file):
-    l = []
-    for i in data:
-        if i["answer"] == "-incorrect":
-            l.append(i)
-    with open("output/{}.json".format(file), "w") as data_file:
-        json.dump(l, data_file, sort_keys=True, indent=4, separators=(',', ': '))
 
-# Searched for a character in the data
-# dummy method
-def deep_analysis_search(data):
-    c = 0
-    for i in data:
-        if i["Result"] == True and '-' in i["Query_Generated"]:
-            c+=1
-            print i["Query_Generated"]
-    print c
+# Get incorrect data and saves them in a json file externally with categoreis
+def deep_analysis(path):
+    result = {}
+    other = []
+    sort = []
+    boolean = []
+    union = []
+    filter = []
+    text = []
+    having = []
+    group = []
+    filter_union = []
+    sum = []
+    num = path[-1]
+
+    for file in os.listdir(path):
+        if file.endswith(".json"):
+            data = load_file("qald/%s/" % num + file)
+            for d in data:
+                pair = {"SPARQL": "", "Question": ""}
+                if d["answer"] == "-without_path":
+                    pair["SPARQL"] = d["query"]
+                    pair["Question"] = d["question"]
+                    if "order" in d["query"].lower():
+                        sort.append(pair)
+                    elif "ask " in d["query"].lower():
+                        boolean.append(pair)
+                    elif "sum" in d["query"].lower():
+                        sum.append(pair)
+                    elif "filter" in d["query"].lower() and "union" in d["query"].lower():
+                        filter_union.append(pair)
+                    elif "filter" in d["query"].lower():
+                        filter.append(pair)
+                    elif "union" in d["query"].lower():
+                        union.append(pair)
+                    elif "text" in d["query"].lower():
+                        text.append(pair)
+                    elif "having" in d["query"].lower():
+                        having.append(pair)
+                    elif "group" in d["query"].lower():
+                        group.append(pair)
+                    else:
+                        other.append(pair)
+    result["Other"] = {"Length": len(other), "Value": other}
+    result["Sort"] = {"Length": len(sort), "Value": sort}
+    result["Boolean"] = {"Length": len(boolean), "Value": boolean}
+    result["Union"] = {"Length": len(union), "Value": union}
+    result["Filter"] = {"Length": len(filter), "Value": filter}
+    result["Text"] = {"Length": len(text), "Value": text}
+    result["Having"] = {"Length": len(having), "Value": having}
+    result["Group"] = {"Length": len(group), "Value": group}
+    result["Filter_Union"] = {"Length": len(filter_union), "Value": filter_union}
+    result["Sum"] = {"Length": len(sum), "Value": sum}
+    with open("output/deep_analysis_qald/qald_%s.json" % num, "w") as data_file:
+        json.dump(result, data_file, sort_keys=True, indent=4, separators=(',', ': '))
+
 
 # Input Question output index in the dataset
 def get_question_index(question):
     ds = LC_Qaud_Linked("./data/LC-QUAD/linked_answer6.json")
     ds.load()
     ds.parse()
-    for i in range(0,len(ds.qapairs)):
+    for i in range(0, len(ds.qapairs)):
         if question == ds.qapairs[i].question.text:
             print "Question id:" ,i
             return i
+
 
 # Input question list, out put query gen
 def get_question_query(questions):
@@ -142,12 +184,14 @@ def compare_query(q1, q2):
     return True
 
 
+# Runs the Query Gen on a Qald Folder
 def analysis_qald(num):
     matches = []
     for root, dir_names, file_names in os.walk('data/QALD'):
-        for filename in fnmatch.filter(file_names, '*.xml'):
-            if "questions" not in filename and num in root:
+        for filename in fnmatch.filter(file_names, '*.json'):
+            if "questions" not in filename and num in root and "raw" not in filename:
                 matches.append(os.path.join(root, filename))
+
     for m in matches:
         print "AA ds", m
         name = re.search(r"(\w+-)*\w+\.\w+", m).group(0)
@@ -172,7 +216,9 @@ if __name__ == "__main__":
 
     # os.system("python query_gen.py --path %s" % "/Users/just3obad/Desktop/Thesis/AskNow/SQG/data/QALD/7/data/qald-7-train-largescale.xml")
     # basic_stats(load_file("tmp"))
-    analysis_x()
+    # analysis_x()
+    # analysis_qald("6")
+    deep_analysis("output/qald/7")
 
     
 
