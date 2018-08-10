888ddf467768d1d48422ae40e7e3179b7aa15b17
just3obad <just3obad@gmail.com>
2018-08-10 15:32:44 +0200

    Syncing

--- datasets_preprocessing.py
+++ datasets_preprocessing.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import json, os, re
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import WordNetLemmatizer
 
 
 def load_json(path):
@@ -19,14 +22,17 @@ def save_json(path, results):
 
 def get_complex_questions(path="./data/clean_datasets/"):
 
-    datasets = []
-    questions_only = []
-    for f in os.listdir(path):
-        if f.endswith("json"):
-            if "dataset" in f:
-                datasets.append(path+f)
-            else:
-                questions_only.append(path+f)
+    # datasets = []
+    # questions_only = []
+    # for f in os.listdir(path):
+    #     if f.endswith("json"):
+    #         if "dataset" in f:
+    #             datasets.append(path+f)
+    #         else:
+    #             questions_only.append(path+f)
+
+    datasets = ['data/clean_datasets/qald_dataset.json', 'data/clean_datasets/lcquad_dataset.json',
+             'data/clean_datasets/dbnqa_dataset.json']
 
     order_questions_labelled = []
     filter_questions_labelled = []
@@ -34,24 +40,28 @@ def get_complex_questions(path="./data/clean_datasets/"):
 
     for file_path in datasets:
         o, f, a = get_labelled_complex_questions(file_path)
-        order_questions_labelled += o
-        filter_questions_labelled += f
-        aggregate_questions_labelled += a
+        order_questions_labelled = o
+        filter_questions_labelled = f
+        aggregate_questions_labelled = a
 
-    save_complex_questions(order_questions_labelled, filter_questions_labelled, aggregate_questions_labelled,
-                           labelled=True)
+        name = re.findall(r"\w*_dataset", file_path)[-1].replace("_dataset", "")
+        # name = ""
 
-    order_questions_manual = []
-    filter_questions_manual = []
-    aggregate_questions_manual = []
+    # print len(order_questions_labelled), len(filter_questions_labelled), len(aggregate_questions_labelled)
+        save_complex_questions(order_questions_labelled, filter_questions_labelled, aggregate_questions_labelled,
+                           labelled="", dataset=name)
 
-    for file_path in questions_only:
-        o, f, a = get_manually_labelled_complex_questions(file_path)
-        order_questions_manual += o
-        filter_questions_manual += f
-        aggregate_questions_manual += a
-
-    save_complex_questions(order_questions_manual, filter_questions_manual, aggregate_questions_manual, False)
+    # order_questions_manual = []
+    # filter_questions_manual = []
+    # aggregate_questions_manual = []
+    #
+    # for file_path in questions_only:
+    #     o, f, a = get_manually_labelled_complex_questions(file_path)
+    #     order_questions_manual += o
+    #     filter_questions_manual += f
+    #     aggregate_questions_manual += a
+    #
+    # save_complex_questions(order_questions_manual, filter_questions_manual, aggregate_questions_manual, False)
 
 
 def get_labelled_complex_questions(path):
@@ -136,20 +146,145 @@ def get_manually_labelled_complex_questions(path):
     return order_questions, filter_questions, aggregate_questions
 
 
-def save_complex_questions(order_q, filter_q, agg_q, labelled=False):
-    if labelled:
+def save_complex_questions(order_q, filter_q, agg_q, labelled="dbpedia", dataset=""):
+    if labelled == "labelled":
         path = "_labelled"
+    elif labelled == "dbpedia":
+        path = "_dbpedia"
     else:
         path = ""
-    save_json("./data/clean_datasets/complex_questions/order_questions{}.json".format(path), order_q)
-    save_json("./data/clean_datasets/complex_questions/filter_questions{}.json".format(path), filter_q)
-    save_json("./data/clean_datasets/complex_questions/aggregate_questions{}.json".format(path), agg_q)
+
+    if dataset != "":
+        dataset = "_{}".format(dataset)
+
+    save_json("./data/clean_datasets/dbpedia/order_questions{}{}.json".format(path, dataset), order_q)
+    save_json("./data/clean_datasets/dbpedia/filter_questions{}{}.json".format(path, dataset), filter_q)
+    save_json("./data/clean_datasets/dbpedia/aggregate_questions{}{}.json".format(path, dataset), agg_q)
 
 
-def basic_stats(path="data/clean_datasets/complex_questions/"):
+def basic_stats(path="data/clean_datasets/complex_questions/", dataset=False):
     for f in os.listdir(path):
         data = load_json(path+f)
         print "Dataset: {} Total No. Questions: {}".format(f, len(data))
+
+
+def deep_stat(path="data/clean_datasets/"):
+    files = []
+    for f in os.listdir(path):
+        if ".json" in f:
+            files.append(path+f)
+
+    # files = ['data/clean_datasets/qald_dataset.json', 'data/clean_datasets/lcquad_dataset.json',
+    #          'data/clean_datasets/dbnqa_dataset.json']
+
+    # files = ['data/clean_datasets/ComplexWeQquestions_dataset.json', 'data/clean_datasets/freebase_dataset.json',
+    #          'data/clean_datasets/webqsp_dataset.json', 'data/clean_datasets/graph_dataset.json',
+    #          'data/clean_datasets/wiki_data_simple_questions_dataset.json']
+
+    for f in files:
+        # get_complexity(f)
+        get_questions_stats(f)
+        # get_linked_items(f)
+
+
+def get_complexity(path):
+    if "dbpedia" not in path:
+        return
+
+    result = {1: 0, 2: 0, 3: 0}
+    data = load_json(path)
+
+    for row in data:
+
+        query = re.findall(r"{.*}", row["query"])
+
+        if query:
+            query = query[0]
+            count = max(query.count("dbo"), query.count("ontology"))
+
+            if count == 0 or count == 1:
+                result[1] += 1
+            elif count == 2:
+                result[2] += 1
+            else:
+                result[3] += 1
+
+    print "Dataset: {} Complexity {}".format(path, result)
+
+
+def get_questions_stats(path):
+    if "dbpedia" not in path:
+        return
+    print path
+    data = load_json(path)
+    no_questions = len(data)
+    words = set()
+    words_total = 0
+    max_question = 0
+    words_lemmated = set()
+
+    stopWords = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
+
+    for row in data:
+        question = word_tokenize(clean_question(row["question"]))
+        words_total += len(question)
+        max_question = max(max_question, len(question))
+
+        for word in question:
+            if word not in stopWords:
+                words.add(word)
+            if lemmatizer.lemmatize(word) not in words_lemmated:
+                words_lemmated.add(word)
+
+    words_length = len(words)
+    words = list(words)
+    if no_questions > 0:
+        average_question = float(words_total) / float(no_questions)
+    else:
+        average_question = 0
+
+    print "Dataset: {}, Average #q: {}, Max #q: {}, Vocabulary: {}, lemmatized Vocab: {}".format(path, average_question,
+            max_question, words_length, len(list(words_lemmated)))
+
+
+def get_linked_items(path):
+    if "dbpedia" not in path:
+        return
+
+    print path
+
+    data = load_json(path)
+    result = {}
+
+    for row in data:
+        query = re.findall(r"{.*}", row["query"])
+
+        if query:
+            query = query[0]
+            count = len(re.findall(r"dbpedia.org|dbo:|dbr:", query))
+            # if count == 0:
+                # print query
+                # print count
+
+            if count in result:
+                result[count] += 1
+            else:
+                result[count] = 1
+    print result
+    return result
+
+
+
+
+def clean_question(q):
+    q = q.lower()
+    q = q.replace(" 's", " is")
+    q = re.sub(r"\?|'|,|\.|`|\\|\"", "", q)
+    q = re.sub(r" {2,}|-", " ", q)
+    q = re.sub(r"\t", " ", q)
+    q = q.strip()
+    return q
 
 
 if __name__ == '__main__':
@@ -157,17 +292,12 @@ if __name__ == '__main__':
     # get_complex_questions()
 
     # basic_stats()
+    deep_stat("data/clean_datasets/combined_datasets/")
+
+
 
     # a, b, c = get_labelled_complex_questions("data/clean_datasets/ComplexWeQquestions_dataset.json")
     # save_complex_questions(a, b, c, True)
     # print a
 
     # get_manually_labelled_complex_questions("data/clean_datasets/QAD_questions.json")
-
-
-
-
-
-
-
-
